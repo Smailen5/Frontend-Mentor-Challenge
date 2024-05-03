3f525f01f92d86aa4842e2629d981a562a075544
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-05-03 17:13:59 +0200

    completato layout mobile

--- intro-component-with-signup-form-master/tailwind.config.js
+++ intro-component-with-signup-form-master/tailwind.config.js
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*.{html,js}"],
   theme: {
     fontFamily: {
-      'poppins': ['Poppins', 'sans-serif']
+      poppins: ["Poppins", "sans-serif"],
     },
     colors: {
-      'red': 'hsl(0, 78%, 62%)',
-      'green': 'hsl(154, 59%, 51%)',
-      'blue': 'hsl(248, 32%, 49%)',
-      'darkBlue': 'hsl(249, 10%, 26%)',
-      'grayishBlue': 'hsl(246, 25%, 77%)',
+      red: "hsl(0, 100%, 74%)",
+      green: "hsl(154, 59%, 51%)",
+      blue: "hsl(248, 32%, 49%)",
+      darkBlue: "hsl(249, 10%, 26%)",
+      gray: "hsl(246, 25%, 77%)",
+      white: "hsl(0, 0%, 100%)",
     },
     fontSize: {
-      'xs': '12px',
-      'sm': '14px',
-      'base': '16px',
-      'lg': '17px',
-      'xl': '20px',
-      '2xl': '50px',
-    }
+      xs: "12px",
+      sm: "14px",
+      base: "17px",
+      lg: "20px",
+      xl: "28px",
+      "2xl": "50px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};

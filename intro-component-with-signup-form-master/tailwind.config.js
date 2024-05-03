f2982f27a592062139783975e26df19dad1dea2f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-05-03 16:40:48 +0200

    installato tailwind

--- intro-component-with-signup-form-master/tailwind.config.js
+++ intro-component-with-signup-form-master/tailwind.config.js
@@ -2,6 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif']
+    },
+    colors: {
+      'red': 'hsl(0, 78%, 62%)',
+      'green': 'hsl(154, 59%, 51%)',
+      'blue': 'hsl(248, 32%, 49%)',
+      'darkBlue': 'hsl(249, 10%, 26%)',
+      'grayishBlue': 'hsl(246, 25%, 77%)',
+    },
+    fontSize: {
+      'xs': '12px',
+      'sm': '14px',
+      'base': '16px',
+      'lg': '17px',
+      'xl': '20px',
+      '2xl': '50px',
+    }
     extend: {},
   },
   plugins: [],

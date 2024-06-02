b4bd7d12c35b4d328057971132a57c3fb4745490
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-06-02 13:00:29 +0200

    - aggiunto nuovo progetto

---
+++ componenet-with-signup-form/src/components/ButtonGreen.jsx
@@ -0,0 +1,14 @@
+function ButtonGreen() {
+  return (
+    <>
+      <button
+        type="submit"
+        className="bg-green p-4 rounded-md shadow-green/50 shadow-1 uppercase"
+      >
+        Claim your free trial
+      </button>
+    </>
+  );
+}
+
+export default ButtonGreen;

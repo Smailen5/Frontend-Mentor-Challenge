7455171eae5bbcd345da816af9d9d70ecb4443e6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-22 14:22:33 +0200

    feat: :lipstick: aggiunge contattami
    aggiunge testo e stile per  contattami

--- single-page-developer-portfolio/src/components/Hero.jsx
+++ single-page-developer-portfolio/src/components/Hero.jsx
@@ -29,6 +29,11 @@ const Hero = () => {
           Based in the UK, I&apos;m a front-end developer passionate about
           building accessible web apps that users love.
         </p>
+        <div className="mt-8">
+          <h3 className="inline-block border-b-2 border-emerald-400 pb-3 text-xl uppercase">
+            Contact me
+          </h3>
+        </div>
       </section>
     </div>
   );

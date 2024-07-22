a50812385ba22d933928f656d3fc4a5377974e1d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-22 13:57:02 +0200

    feat: :lipstick: aggiunge testo
    aggiunte sezione presentazione con stile di base

--- single-page-developer-portfolio/src/components/Hero.jsx
+++ single-page-developer-portfolio/src/components/Hero.jsx
@@ -4,7 +4,8 @@ import circle from "../assets/images/pattern-circle.svg";
 
 const Hero = () => {
   return (
-    <div className="relative overflow-hidden">
+    <div className="relative overflow-hidden font-spaceGrotesk">
+      {/* sezione immagini */}
       <div className="relative top-0 -z-20 mx-auto flex flex-col items-center h-80">
         <img
           src={rings}
@@ -12,8 +13,20 @@ const Hero = () => {
           className="absolute -left-64 top-4 -z-10 scale-125"
         />
         <img src={imageProfile} alt="alt" className="absolute -top-36 w-52" />
-        <img src={circle} alt="circle" className="absolute -right-16 bottom-0" />
+        <img
+          src={circle}
+          alt="circle"
+          className="absolute -right-16 bottom-0"
+        />
       </div>
+      {/* sezione presentazione */}
+      <section className="text-center pt-8 px-10">
+        <h2 className="text-4xl font-semibold">Nice to meet you! I&apos;m Adam Keyes.</h2>
+        <p className="mt-8 text-neutral-400">
+          Based in the UK, I&apos;m a front-end developer passionate about
+          building accessible web apps that users love.
+        </p>
+      </section>
     </div>
   );
 };

ea6c6aff5afaf62ae17311dac2f167d8b50f72de
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-22 14:24:23 +0200

    feat: :construction: crea Experiences
    aggiunge il componente Experineces

--- single-page-developer-portfolio/src/App.jsx
+++ single-page-developer-portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 
+import Experiences from './components/Experiences'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Experiences />
     </>
   )
 }

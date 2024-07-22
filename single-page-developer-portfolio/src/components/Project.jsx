f6af59f3a8e594274aa2612df383ad7eefc7894b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-22 15:07:00 +0200

    feat: :construction: crea Project.jsx
    aggiunge il componente Project per mantenere il codice pulito

---
+++ single-page-developer-portfolio/src/components/Project.jsx
@@ -0,0 +1,9 @@
+// singolo progetto
+
+const Project = () => {
+  return (
+    <div>Project</div>
+  )
+}
+
+export default Project
--- single-page-developer-portfolio/src/components/Projects.jsx
+++ single-page-developer-portfolio/src/components/Projects.jsx
@@ -1,9 +1,17 @@
-
+/* eslint-disable no-unused-vars */
+import { ContactMe } from "./ContactMe";
+import Project from "./Project";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="font-spaceGrotesk px-4">
+      <header className="flex items-center justify-between mb-8">
+        <h2 className="text-4xl font-semibold">Projects</h2>
+        <ContactMe>Contact me</ContactMe>
+      </header>
 
-export default Projects
+    </div>
+  );
+};
+
+export default Projects;

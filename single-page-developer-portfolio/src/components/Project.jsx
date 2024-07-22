71d88b3b39643f492d22d2a19fe5bc63ef0c7a07
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-22 15:40:37 +0200

    refactor: :art: struttura Project.jsx
    da la struttura di base del componente Project.jsx

--- single-page-developer-portfolio/src/components/Project.jsx
+++ single-page-developer-portfolio/src/components/Project.jsx
@@ -2,15 +2,21 @@
 /* eslint-disable react/prop-types */
 // singolo progetto
 import images from "../data/images.js";
-
+import { ContactMe } from "./ContactMe";
 
 const Project = ({ image, title, technologies, liveSite, github }) => {
-    // console.log(image);
+  // console.log(image);
   return (
     <article>
-        <img src={images[image]} alt={title} className="w-full "/>
+      <img src={images[image]} alt={title} className="w-full" />
+      <h4>{title}</h4>
+      <h5>{technologies}</h5>
+      <div className="flex justify-between pb-4">
+        <ContactMe>view project</ContactMe>
+        <ContactMe>view code</ContactMe>
+      </div>
     </article>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;

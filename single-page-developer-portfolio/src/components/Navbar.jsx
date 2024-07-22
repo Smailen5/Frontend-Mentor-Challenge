627990b45d45165aeb5193d7cd60006994f3b179
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-22 13:00:00 +0200

    refactor: :truck: sposta le icone
    sposta i file delle icone nella cartella asset del progetto

--- single-page-developer-portfolio/src/components/Navbar.jsx
+++ single-page-developer-portfolio/src/components/Navbar.jsx
@@ -1,9 +1,22 @@
+/* eslint-disable no-unused-vars */
+import iconFrontend from "../assets/icon-frontend-mentor.svg";
+import iconGithub from "../assets/icon-github.svg";
+import iconLinkedin from "../assets/icon-linkedin.svg";
+import iconTwitter from "../assets/icon-twitter.svg";
+
 const Navbar = () => {
   const brandLogo = "adamkeyes";
   return (
-    <div>
-      <header className="">{brandLogo}</header>
-    </div>
+    <nav className="flex flex-col items-center py-4 gap-6">
+      <header className="font-semibold text-2xl">{brandLogo}</header>
+      {/* icone social */}
+      <div className="flex gap-4">
+        <img src={iconGithub} alt="alt" />
+        <img src={iconFrontend} alt="alt" />
+        <img src={iconLinkedin} alt="alt" />
+        <img src={iconTwitter} alt="alt" />
+      </div>
+    </nav>
   );
 };
 

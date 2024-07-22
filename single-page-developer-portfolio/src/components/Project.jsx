599b04ee29c0bbebb215e0e3d2de498eceae6112
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-22 15:22:58 +0200

    feat: :construction: aggiunge i progetti
    aggiunge i progetti dinamicamente prendendo i dati dal json

--- single-page-developer-portfolio/src/components/Project.jsx
+++ single-page-developer-portfolio/src/components/Project.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 // singolo progetto
 
-const Project = () => {
+const Project = ({ image, title, technologies, liveSite, github }) => {
   return (
     <div>Project</div>
   )
--- single-page-developer-portfolio/src/components/Projects.jsx
+++ single-page-developer-portfolio/src/components/Projects.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable no-unused-vars */
 import { ContactMe } from "./ContactMe";
 import Project from "./Project";
+import projects from "../data/projects.json";
 
 const Projects = () => {
   return (
-    <div className="font-spaceGrotesk px-4">
-      <header className="flex items-center justify-between mb-8">
+    <div className="px-4 font-spaceGrotesk">
+      <header className="mb-8 flex items-center justify-between">
         <h2 className="text-4xl font-semibold">Projects</h2>
         <ContactMe>Contact me</ContactMe>
       </header>
-
+      <section>
+        {projects.map((project) => (
+          <Project key={project.id} {...project} />
+        ))}
+      </section>
     </div>
   );
 };

02054172093b37aef02c4e30a472c2541bc6e8ed
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-22 14:34:43 +0200

    feat: :construction: aggiunge experiences.json
    aggiunge file json per non ripetere il codice

--- single-page-developer-portfolio/src/components/Experiences.jsx
+++ single-page-developer-portfolio/src/components/Experiences.jsx
@@ -1,9 +1,36 @@
-
-
 const Experiences = () => {
   return (
-    <div>Experiences</div>
-  )
-}
+    <div className="mt-8 px-4">
+      <hr />
+      <section>
+        <article>
+            <h4>html</h4>
+            <h5>4 Years Experience</h5>
+        </article>
 
-export default Experiences
+        <article>
+            <h4>html</h4>
+            <h5>4 Years Experience</h5>
+        </article>
+
+        <article>
+            <h4>html</h4>
+            <h5>4 Years Experience</h5>
+        </article>
+
+        <article>
+            <h4>html</h4>
+            <h5>4 Years Experience</h5>
+        </article>
+
+        <article>
+            <h4>html</h4>
+            <h5>4 Years Experience</h5>
+        </article>
+      </section>
+      <hr />
+    </div>
+  );
+};
+
+export default Experiences;

d2546f0d4b5eb001de512869e9186a430b204bc0
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-23 14:02:10 +0200

    refactor: :recycle: cambia struttura
    cambia struttura images.js per permettere l'utilizzo delle immagini dinamicamente

--- single-page-developer-portfolio/src/data/images.js
+++ single-page-developer-portfolio/src/data/images.js
@@ -1,17 +1,41 @@
-import thumbnailProject1 from "../assets/images/thumbnail-project-1-small.webp";
-import thumbnailProject2 from "../assets/images/thumbnail-project-2-small.webp";
-import thumbnailProject3 from "../assets/images/thumbnail-project-3-small.webp";
-import thumbnailProject4 from "../assets/images/thumbnail-project-4-small.webp";
-import thumbnailProject5 from "../assets/images/thumbnail-project-5-small.webp";
-import thumbnailProject6 from "../assets/images/thumbnail-project-6-small.webp";
+import thumbnailProject1Small from "../assets/images/thumbnail-project-1-small.webp";
+import thumbnailProject1Large from "../assets/images/thumbnail-project-1-large.webp";
+import thumbnailProject2Small from "../assets/images/thumbnail-project-2-small.webp";
+import thumbnailProject2Large from "../assets/images/thumbnail-project-2-large.webp";
+import thumbnailProject3Small from "../assets/images/thumbnail-project-3-small.webp";
+import thumbnailProject3Large from "../assets/images/thumbnail-project-3-large.webp";
+import thumbnailProject4Small from "../assets/images/thumbnail-project-4-small.webp";
+import thumbnailProject4Large from "../assets/images/thumbnail-project-4-Large.webp";
+import thumbnailProject5Small from "../assets/images/thumbnail-project-5-small.webp";
+import thumbnailProject5Large from "../assets/images/thumbnail-project-5-large.webp";
+import thumbnailProject6Small from "../assets/images/thumbnail-project-6-small.webp";
+import thumbnailProject6Large from "../assets/images/thumbnail-project-6-large.webp";
 
 const images = {
-  thumbnailProject1,
-  thumbnailProject2,
-  thumbnailProject3,
-  thumbnailProject4,
-  thumbnailProject5,
-  thumbnailProject6
+  thumbnailProject1: {
+    small: thumbnailProject1Small,
+    large: thumbnailProject1Large,
+  },
+  thumbnailProject2: {
+    small: thumbnailProject2Small,
+    large: thumbnailProject2Large,
+  },
+  thumbnailProject3: {
+    small: thumbnailProject3Small,
+    large: thumbnailProject3Large,
+  },
+  thumbnailProject4: {
+    small: thumbnailProject4Small,
+    large: thumbnailProject4Large,
+  },
+  thumbnailProject5: {
+    small: thumbnailProject5Small,
+    large: thumbnailProject5Large,
+  },
+  thumbnailProject6: {
+    small: thumbnailProject6Small,
+    large: thumbnailProject6Large,
+  },
 };
 
 export default images;

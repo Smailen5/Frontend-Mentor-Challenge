fe75398242e72e99c80746fd58fa36f874bc209c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-23 16:00:07 +0200

    style: :iphone: posiziona experiences
    posiziona il componente experiences con grid e flex in modalita tablet

--- single-page-developer-portfolio/src/components/Experiences.jsx
+++ single-page-developer-portfolio/src/components/Experiences.jsx
@@ -4,19 +4,21 @@ import rings from "../assets/images/pattern-rings.svg";
 const Experiences = () => {
   return (
     // controlla il mt-80 quando hai finito con Hero
-    <div className="py-20 px-4 font-spaceGrotesk relative overflow-x-hidden md:mt-80">
+    <div className="py-20 px-4 font-spaceGrotesk relative overflow-x-hidden md:mt-80 md:px-10">
       <hr />
-      <section className="my-10 flex flex-col items-center gap-8 text-center">
+      <section className="my-10 flex flex-col items-center gap-8 text-center md:grid md:grid-cols-2 md:justify-items-start">
         {experiences.map((experience) => {
           const { skill, years } = experience;
           return (
-            <article key={skill} className="flex flex-col gap-2">
+            <article key={skill} className="flex flex-col gap-2 md:items-start">
               <h4 className="text-4xl font-semibold">{skill}</h4>
               <h5 className="text-neutral-400">{years}</h5>
             </article>
           );
         })}
       </section>
+      {/* devo mettere la funzione handleResize in un context cosi posso usarla anche qui */}
+      {/* {window.innerWidth <= 768 && <hr/>} */}
       <hr />
       <img src={rings} alt="rings" className="absolute -right-72 bottom-7 -z-10 scale-125" />
     </div>

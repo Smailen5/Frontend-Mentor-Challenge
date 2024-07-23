550a804f527d2616e5f2fa407ff630252f859015
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-23 12:56:09 +0200

    feat: :construction: crea Footer
    crea il componente Footer clonando Navbar

---
+++ single-page-developer-portfolio/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import iconFrontend from "../assets/icon-frontend-mentor.svg";
+import iconGithub from "../assets/icon-github.svg";
+import iconLinkedin from "../assets/icon-linkedin.svg";
+import iconTwitter from "../assets/icon-twitter.svg";
+
+const Navbar = () => {
+  const brandLogo = "adamkeyes";
+  return (
+    <nav className="flex flex-col items-center gap-6 py-6">
+      <header className="font-spaceGrotesk text-2xl font-semibold">
+        {brandLogo}
+      </header>
+      {/* icone social */}
+      <div className="flex gap-4">
+        <img src={iconGithub} alt="alt" />
+        <img src={iconFrontend} alt="alt" />
+        <img src={iconLinkedin} alt="alt" />
+        <img src={iconTwitter} alt="alt" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

870c0d9474442a536c4bc1d0b04625f5fbebebae
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-23 15:29:42 +0200

    refactor: :iphone: aggiunge br
    aggiunge br dinamicamente in base alla larghezza dello schermo

--- single-page-developer-portfolio/src/components/Hero.jsx
+++ single-page-developer-portfolio/src/components/Hero.jsx
@@ -2,8 +2,24 @@ import rings from "../assets/images/pattern-rings.svg";
 import circle from "../assets/images/pattern-circle.svg";
 import Button from "./Button";
 import images from "../data/images.js";
+import { useEffect, useState } from "react";
+
+// controlla se cambia la larghezza e ritorna la larghezza attuale
+const useWindowWidth = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return width;
+};
 
 const Hero = () => {
+  const width = useWindowWidth();
+  const isTablet = width >= 768;
+
   return (
     <div className="relative top-36 overflow-x-clip font-spaceGrotesk md:top-36">
       {/* sezione immagini */}
@@ -35,10 +51,10 @@ const Hero = () => {
 
       {/* sezione presentazione */}
       <section className="px-10 pt-8 text-center md:absolute md:top-0 md:w-4/5 md:text-left">
-        <h2 className="text-4xl font-semibold md:text-7xl">
-          Nice to meet you! I&apos;m{" "}
-          <span className="border-b-4 border-emerald-400">Adam Keyes</span>.
-        </h2>
+          <h2 className="text-4xl font-semibold md:text-8xl">
+            Nice to {isTablet && <br />} meet you! I&apos;m{" "}
+            <span className="border-b-4 border-emerald-400">Adam Keyes</span>.
+          </h2>
         <p className="mt-8 text-lg text-neutral-400">
           Based in the UK, I&apos;m a front-end developer passionate about
           building accessible web apps that users love.

640680556b2983a438cfe5cc7636f26236a166ee
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-24 12:37:33 +0200

    fix: :bug: fix con tailwind merge
    modifica il componente per tailwind merge

--- single-page-developer-portfolio/src/components/Button.jsx
+++ single-page-developer-portfolio/src/components/Button.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
-const Button = ({ children, type, classButton }) => {
+import { twMerge } from "tailwind-merge";
+const Button = ({ children, type, className }) => {
   // ricordati di modificare il mt-8 in mt-4 e di adeguare il codice dove viene usato il componente
   return (
     <button
       type={type}
       className={twMerge(
-        `inline-block border-b-2 border-emerald-400 pb-3 text-xl uppercase md:text-2xl`
-        
-      ) className}
+        "inline-block border-b-2 border-emerald-400 pb-3 text-xl uppercase md:text-2xl",
+        className,
+      )}
     >
       {children}
     </button>

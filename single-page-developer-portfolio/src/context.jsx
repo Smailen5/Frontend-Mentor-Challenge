e5fe1e05fb5433f8d869a70df348cabcaee7e7e5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-24 13:35:01 +0200

    docs: :bulb: commenta codice
    spiega il funzionamento della funzione useWindowWidth

--- single-page-developer-portfolio/src/context.jsx
+++ single-page-developer-portfolio/src/context.jsx
@@ -4,21 +4,24 @@ import { useState, useEffect } from "react";
 
 const AppContext = createContext();
 
+// controlla larghezza schermo e ne ritorna il valore
 const useWindowWidth = () => {
-    const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
-      const handleResize = () => setWidth(window.innerWidth);
-      window.addEventListener("resize", handleResize);
-      return () => window.removeEventListener("resize", handleResize);
-    }, []);
-    return width;
-  };
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return width;
+};
+
 const AppProvider = ({ children }) => {
-  
-
-
-  return <AppContext.Provider value={{ useWindowWidth }}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ useWindowWidth }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 const useGlobalContext = () => {

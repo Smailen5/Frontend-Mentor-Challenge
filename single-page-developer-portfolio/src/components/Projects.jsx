bf13841e8a6721b4a0baf736cb41098c7505294b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-24 11:55:18 +0200

    style: aumenta padding

--- single-page-developer-portfolio/src/components/Projects.jsx
+++ single-page-developer-portfolio/src/components/Projects.jsx
@@ -5,7 +5,7 @@ import projects from "../data/projects.json";
 
 const Projects = () => {
   return (
-    <div className="px-4 font-spaceGrotesk pt-4">
+    <div className="px-4 pt-4 md:px-10">
       <header className="mb-8 flex items-center justify-between">
         <h2 className="text-4xl font-semibold">Projects</h2>
         <Button>Contact me</Button>

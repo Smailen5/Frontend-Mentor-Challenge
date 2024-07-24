379083a7cc4711419f9476f3f585691df1169767
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-24 13:18:58 +0200

    style: :iphone: aumenta padding
    aumenta padding laterale in modalita desktop

--- single-page-developer-portfolio/src/components/Navbar.jsx
+++ single-page-developer-portfolio/src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { github, frontend, twitter, linkedin } from "../data/socialIcon";
 const Navbar = () => {
   const brandLogo = "adamkeyes";
   return (
-    <nav className="absolute flex w-full flex-col items-center pt-6 lg:pt-10">
-      <div className="absolute flex w-full flex-col items-center gap-6 px-10 md:flex-row md:justify-between lg:px-56">
-        <header className="font-spaceGrotesk text-2xl font-semibold">
+    <nav className="absolute flex w-full flex-col items-center pt-6 xl:pt-10">
+      <div className="absolute flex w-full flex-col items-center gap-6 px-10 md:flex-row md:justify-between xl:px-56">
+        <header className="text-2xl font-semibold md:text-4xl">
           {brandLogo}
         </header>
         {/* icone social */}

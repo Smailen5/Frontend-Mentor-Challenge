ec740d7078eeac60f435838666b44b16ecec2c97
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-25 15:41:18 +0200

    style: :iphone: cambia @meadia
    cambia media query per le immagini e aggiunge margine inferiore in modalita desktop

--- single-page-developer-portfolio/src/components/Project.jsx
+++ single-page-developer-portfolio/src/components/Project.jsx
@@ -8,9 +8,9 @@ import Button from "./Button.jsx";
 const Project = ({ image, title, technologies, liveSite, github }) => {
   // console.log(image);
   return (
-    <article>
+    <article className="xl:mb-10">
       <picture>
-        <source media="(min-width: 768px)" srcSet={images[image].large} />
+        <source media="(min-width: 1280px)" srcSet={images[image].large} />
         <img src={images[image].small} alt={title} className="w-full" />
       </picture>
       <h4 className="pb-4 pt-6 text-3xl font-bold uppercase">{title}</h4>
@@ -23,7 +23,7 @@ const Project = ({ image, title, technologies, liveSite, github }) => {
           );
         })}
       </div>
-      <div className="flex gap-8 pb-12 pt-6">
+      <div className="flex gap-8 pb-12 pt-6 xl:hidden">
         <Button>view project</Button>
         <Button>view code</Button>
       </div>

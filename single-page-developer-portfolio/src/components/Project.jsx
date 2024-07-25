9549dad4255cbbbca48aae9709e3af035bdadc06
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-25 16:53:29 +0200

    feat: :lipstick: aggiunge overlay
    aggiunge overlay immagini con bottoni all'interno solo in modalita desktop

--- single-page-developer-portfolio/src/components/Project.jsx
+++ single-page-developer-portfolio/src/components/Project.jsx
@@ -9,9 +9,15 @@ const Project = ({ image, title, technologies, liveSite, github }) => {
   // console.log(image);
   return (
     <article className="xl:mb-10">
-      <picture>
+      <picture className="group xl:relative">
         <source media="(min-width: 1280px)" srcSet={images[image].large} />
         <img src={images[image].small} alt={title} className="w-full" />
+        <div className="absolute right-0 top-0 h-full max-w-full w-full place-items-center bg-black/70 opacity-0 transition-opacity duration-500 grid group-[&:hover]:opacity-100">
+          <div className="flex flex-col items-center gap-14">
+            <Button>view project</Button>
+            <Button>view code</Button>
+          </div>
+        </div>
       </picture>
       <h4 className="pb-4 pt-6 text-3xl font-bold uppercase">{title}</h4>
       <div className="flex gap-4">

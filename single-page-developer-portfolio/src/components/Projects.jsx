26780a45199161f4c205340f64e654f0d45041a1
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-25 15:38:33 +0200

    style: :iphone: aumenta testo
    aumenta grandezza testo header projects e aumenta margine inferiore

--- single-page-developer-portfolio/src/components/Projects.jsx
+++ single-page-developer-portfolio/src/components/Projects.jsx
@@ -5,9 +5,9 @@ import projects from "../data/projects.json";
 
 const Projects = () => {
   return (
-    <div className="px-4 pt-4 md:px-10 md:pt-12">
-      <header className="mb-8 flex items-center justify-between md:mb-16">
-        <h2 className="text-4xl font-semibold md:text-7xl">Projects</h2>
+    <div className="px-4 pt-4 md:px-10 md:pt-12 xl:px-56">
+      <header className="mb-8 flex items-center justify-between md:mb-16 xl:mb-24">
+        <h2 className="text-4xl font-semibold md:text-7xl xl:text-8xl">Projects</h2>
         <Button>Contact me</Button>
       </header>
       <section className="grid grid-cols-1 gap-8 md:grid-cols-2 md:mb-12">

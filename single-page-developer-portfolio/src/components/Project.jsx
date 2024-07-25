435854c0ef63cbf078bf492863b257c7f4df4f5b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-07-25 17:04:09 +0200

    fix: :lipstick: render condizionale
    render condizionale del overlay solo se lo schermo e desktop. risolve comparsa overlay su schermi piu piccoli. risolve comparsa overlay quando gia gli altri bottoni sono visibili

--- single-page-developer-portfolio/src/components/Project.jsx
+++ single-page-developer-portfolio/src/components/Project.jsx
@@ -4,20 +4,27 @@
 import React from "react";
 import images from "../data/images.js";
 import Button from "./Button.jsx";
+import { useGlobalContext } from "../context";
 
 const Project = ({ image, title, technologies, liveSite, github }) => {
+  const { useWindowWidth } = useGlobalContext();
+  const windowWidth = useWindowWidth();
+  const isDesktop = windowWidth >= 1280;
   // console.log(image);
   return (
     <article className="xl:mb-10">
       <picture className="group xl:relative">
         <source media="(min-width: 1280px)" srcSet={images[image].large} />
         <img src={images[image].small} alt={title} className="w-full" />
-        <div className="absolute right-0 top-0 h-full max-w-full w-full place-items-center bg-black/70 opacity-0 transition-opacity duration-500 grid group-[&:hover]:opacity-100">
+        {isDesktop && (
+          <div className="absolute right-0 top-0 grid h-full w-full max-w-full place-items-center bg-black/70 opacity-0 transition-opacity duration-500 group-[&:hover]:opacity-100">
           <div className="flex flex-col items-center gap-14">
             <Button>view project</Button>
             <Button>view code</Button>
           </div>
         </div>
+        )}
+        
       </picture>
       <h4 className="pb-4 pt-6 text-3xl font-bold uppercase">{title}</h4>
       <div className="flex gap-4">

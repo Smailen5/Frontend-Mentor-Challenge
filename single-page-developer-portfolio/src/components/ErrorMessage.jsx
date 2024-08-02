777cf3023d425a10bf436b04de49c83d49a3ffb5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-02 15:39:26 +0200

    style: :lipstick: aggiunge classe
    aggiunge classe per lo stile del componente

--- single-page-developer-portfolio/src/components/ErrorMessage.jsx
+++ single-page-developer-portfolio/src/components/ErrorMessage.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
 
 export default function ErrorMessage({ children }){
-    return <p className="text-red-500">{children}</p>
+    return <p className="text-red-500 text-right">{children}</p>
 }

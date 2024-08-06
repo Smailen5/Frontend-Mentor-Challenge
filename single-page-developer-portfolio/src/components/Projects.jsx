0d621fdb4a3b3c56bc5af5a4d53f47ed6a875872
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-06 17:10:15 +0200

    style: :iphone: aumenta margini

--- single-page-developer-portfolio/src/components/Projects.jsx
+++ single-page-developer-portfolio/src/components/Projects.jsx
@@ -6,14 +6,14 @@ import projects from "../data/projects.json";
 
 const Projects = () => {
   return (
-    <section className="m-4">
+    <section className="m-4 md:mx-8">
       <header className="flex items-center justify-between">
         <h3 className="text-3xl font-bold md:text-7xl">Projects</h3>
         <Button href="#" type="button">
           contact me
         </Button>
       </header>
-      <section className="my-8 flex flex-col gap-8 md:grid md:grid-cols-2">
+      <section className="my-8 flex flex-col gap-8 md:grid md:grid-cols-2 md:my-12 md:gap-y-12">
         {projects.map((project) => (
           <Project key={project.id} {...project} />
         ))}

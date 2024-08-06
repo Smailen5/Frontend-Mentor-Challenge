14fce69f8b8c3b0769215cebbda7fab0bd633ca1
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-06 17:05:10 +0200

    style: :iphone: riodina progetti
    riordina progetti in colonna con grid e aumenta grandezza testo in modalita desktop

--- single-page-developer-portfolio/src/components/Projects.jsx
+++ single-page-developer-portfolio/src/components/Projects.jsx
@@ -8,12 +8,12 @@ const Projects = () => {
   return (
     <section className="m-4">
       <header className="flex items-center justify-between">
-        <h3 className="text-3xl font-bold">Projects</h3>
+        <h3 className="text-3xl font-bold md:text-7xl">Projects</h3>
         <Button href="#" type="button">
           contact me
         </Button>
       </header>
-      <section className="my-8 flex flex-col gap-8">
+      <section className="my-8 flex flex-col gap-8 md:grid md:grid-cols-2">
         {projects.map((project) => (
           <Project key={project.id} {...project} />
         ))}

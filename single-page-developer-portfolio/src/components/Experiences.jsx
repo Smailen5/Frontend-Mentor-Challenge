9f43eaa24739270e94a62531521876c977d17e75
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-06 19:56:59 +0200

    style: :iphone: riordina griglia
    riordina griglia experiences per schermi >1440px

--- single-page-developer-portfolio/src/components/Experiences.jsx
+++ single-page-developer-portfolio/src/components/Experiences.jsx
@@ -10,13 +10,13 @@ const Experiences = () => {
   return (
     <div className="relative overflow-x-clip py-8 md:py-16 xl:py-24">
       <hr className="mx-4 md:mx-8 xl:mx-36" />
-      <div className="flex flex-col gap-8 py-8 text-center md:mx-8 md:grid md:grid-cols-2 md:gap-12 md:text-left">
+      <div className="flex flex-col gap-8 py-8 text-center md:mx-8 md:grid md:grid-cols-2 md:gap-12 md:text-left xl:mx-36 xl:grid-cols-3 xl:py-16">
         {experiences.map((experience) => (
           <div key={experience.skill}>
-            <h3 className="text-3xl font-bold md:text-4xl">
+            <h3 className="text-3xl font-bold md:text-4xl xl:text-5xl">
               {experience.skill}
             </h3>
-            <h4 className="mt-2 text-skin-variant md:mt-6">
+            <h4 className="mt-2 text-skin-variant md:mt-6 xl:text-xl">
               {experience.years}
             </h4>
           </div>
@@ -26,7 +26,7 @@ const Experiences = () => {
       <img
         src={rings}
         alt="icon rings"
-        className="absolute -right-1/3 bottom-0 translate-x-1/3 md:-right-2/3 md:bottom-6 md:-translate-x-1/3 lg:-right-1/2 lg:-translate-x-1/2"
+        className="absolute -right-1/3 bottom-0 translate-x-1/3 md:-right-2/3 md:bottom-6 md:-translate-x-1/3 lg:-right-1/2 lg:-translate-x-1/2 xl:-right-1/3 xl:-translate-x-1/3 xl:bottom-28"
       />
     </div>
   );

8d969d163d7fa28ac7fd1363d321241b0b98d41c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-07 16:58:08 +0200

    docs: :memo: aggiunge gradienti
    aggiunge variabili per gradienti

--- base-apparel-coming/src/App.jsx
+++ base-apparel-coming/src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,7 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className=" bg-gradient-primary">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -28,7 +28,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- base-apparel-coming/tailwind.config.js
+++ base-apparel-coming/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--clr-desaturated-red)",
+        secondary: "var(--clr-soft-red)",
+        neutral: "var(--clr-neutral)",
+        gradientPrimaryFrom: "var(--clr-gradient-primary-from)",
+        gradientPrimaryTo: "var(--clr-gradient-primary-to)",
+        gradientSecondaryFrom: "var(--clr-gradient-secondary-from)",
+        gradientSecondaryTo: "var(--clr-gradient-secondary-to)",
+      },
+      backgroundImage: {
+        "gradient-primary":
+          "linear-gradient(135deg, var(--clr-gradient-primary-from), var(--clr-gradient-primary-to))",
+        "gradient-secondary":
+          "linear-gradient(135deg, var(--clr-gradient-secondary-from), var(--clr-gradient-secondary-to))",
+      },
+    },
   },
   plugins: [],
-}
+};

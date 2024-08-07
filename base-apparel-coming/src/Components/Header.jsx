59f913b893d6b0d4cf822cc087caceb354107139
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-07 23:10:28 +0200

    style: :sparkles: aggiunge twMerge
    aggiunge tailwind merge a header

--- base-apparel-coming/src/Components/Header.jsx
+++ base-apparel-coming/src/Components/Header.jsx
@@ -1,6 +1,9 @@
-const Header = () => {
+/* eslint-disable react/prop-types */
+import { twMerge } from "tailwind-merge"
+
+const Header = ({ className }) => {
   return (
-    <header className="bg-transparent w-full p-8 xl:hidden">
+    <header className={twMerge("bg-transparent w-full p-8 xl:hidden", className)}>
       <div>
         <img src="/images/logo.svg" alt="logo" className="w-32" />
       </div>

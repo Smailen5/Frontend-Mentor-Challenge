18685e581b5868fdb4eb37bf1d4f2e5080bfae43
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-07 14:49:49 +0200

    style: :iphone: aggiunge padding

--- single-page-developer-portfolio/src/components/Footer.jsx
+++ single-page-developer-portfolio/src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className="flex flex-col gap-8 w-full">
         <hr />
-      <div className="flex flex-col items-center justify-center gap-4 bg-skin-semiBlack pb-12 md:w-full md:flex-row md:justify-between">
+      <div className="flex flex-col items-center justify-center gap-4 bg-skin-semiBlack pb-12 md:w-full md:flex-row md:justify-between xl:pb-20">
         <header className="text-2xl font-bold md:text-3xl">{brandLogo()}</header>
         {/* icone social */}
         <div className="flex gap-8 text-xl md:text-2xl">

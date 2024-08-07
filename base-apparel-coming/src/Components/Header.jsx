ee672d1cf5d253255a5b548cd1cd6e4a2bca1ac4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-07 19:35:34 +0200

    refactor: :construction: cambia posizionamento
    cambia posizionamento header in fixed

--- base-apparel-coming/src/Components/Header.jsx
+++ base-apparel-coming/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="w-full bg-gradient-primary p-8">
+    <header className="w-full bg-gradient-primary p-8 fixed">
       <div>
         <img src="/images/logo.svg" alt="logo" className="w-32" />
       </div>
--- base-apparel-coming/src/Components/Hero.jsx
+++ base-apparel-coming/src/Components/Hero.jsx
@@ -3,7 +3,11 @@ const Hero = () => {
     <section className="w-full">
       <picture>
         <source media="(min-width: 768px)" srcSet="/images/hero-desktop.jpg" />
-        <img src="/images/hero-mobile.jpg" alt="hero" />
+        <img
+          src="/images/hero-mobile.jpg"
+          alt="hero"
+          className="w-full pt-[5.5rem]"
+        />
       </picture>
     </section>
   );

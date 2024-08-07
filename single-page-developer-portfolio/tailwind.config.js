17164a57e3154e3382f2e6695ea66fe62035a236
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-07 14:55:50 +0200

    docs: :memo: aggiunge breakpoint
    aggiunge 2xl ai breakpoint disponibili

--- single-page-developer-portfolio/tailwind.config.js
+++ single-page-developer-portfolio/tailwind.config.js
@@ -1,47 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontSize:{
-        '4.5xl': ['2.5rem', '2.75rem'],
-        '7xl': ['4.5rem', '4.5rem'],
-        '8xl': ['5.60rem', '6rem'],
+      screens: {
+        "2xl": "2560px",
+      },
+      fontSize: {
+        "4.5xl": ["2.5rem", "2.75rem"],
+        "7xl": ["4.5rem", "4.5rem"],
+        "8xl": ["5.60rem", "6rem"],
       },
       fontFamily: {
-        spaceGrotesk: ['Space Grotesk', 'sans-serif'],
+        spaceGrotesk: ["Space Grotesk", "sans-serif"],
       },
       textColor: {
         skin: {
-          base: 'var(--color-text-primary)',
-          variant: 'var(--color-text-secondary)',
-          accent: 'var(--color-accent-secondary)',
-          error: 'var(--color-text-error)',
-        }
+          base: "var(--color-text-primary)",
+          variant: "var(--color-text-secondary)",
+          accent: "var(--color-accent-secondary)",
+          error: "var(--color-text-error)",
+        },
       },
-      backgroundColor:{
+      backgroundColor: {
         skin: {
-          base: 'var(--color-bg-primary)',
-          semiBlack: 'var(--color-bg-secondary)',
-          accent: 'var(--color-accent-secondary)',
-        }
+          base: "var(--color-bg-primary)",
+          semiBlack: "var(--color-bg-secondary)",
+          accent: "var(--color-accent-secondary)",
+        },
       },
-      borderColor:{
+      borderColor: {
         skin: {
-          base: 'var(--color-accent-primary)',
-          accent: 'var(--color-accent-secondary)',
-          error: 'var(--color-text-error)',
-        }
+          base: "var(--color-accent-primary)",
+          accent: "var(--color-accent-secondary)",
+          error: "var(--color-text-error)",
+        },
       },
       fill: {
         skin: {
-          accent: 'var(--color-accent-secondary)',
-        }
-      }
+          accent: "var(--color-accent-secondary)",
+        },
+      },
     },
   },
   plugins: [],
-}
+};

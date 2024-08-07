a88614558dad7f07f21881992b59195de8a7c580
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-07 16:59:22 +0200

    docs: :memo: esclude colori
    sposta le variabili per escludere i colori inclusi con tailwind

--- base-apparel-coming/tailwind.config.js
+++ base-apparel-coming/tailwind.config.js
@@ -2,23 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "var(--clr-desaturated-red)",
-        secondary: "var(--clr-soft-red)",
-        neutral: "var(--clr-neutral)",
-        gradientPrimaryFrom: "var(--clr-gradient-primary-from)",
-        gradientPrimaryTo: "var(--clr-gradient-primary-to)",
-        gradientSecondaryFrom: "var(--clr-gradient-secondary-from)",
-        gradientSecondaryTo: "var(--clr-gradient-secondary-to)",
-      },
-      backgroundImage: {
-        "gradient-primary":
-          "linear-gradient(135deg, var(--clr-gradient-primary-from), var(--clr-gradient-primary-to))",
-        "gradient-secondary":
-          "linear-gradient(135deg, var(--clr-gradient-secondary-from), var(--clr-gradient-secondary-to))",
-      },
+    colors: {
+      primary: "var(--clr-desaturated-red)",
+      secondary: "var(--clr-soft-red)",
+      neutral: "var(--clr-neutral)",
+      gradientPrimaryFrom: "var(--clr-gradient-primary-from)",
+      gradientPrimaryTo: "var(--clr-gradient-primary-to)",
+      gradientSecondaryFrom: "var(--clr-gradient-secondary-from)",
+      gradientSecondaryTo: "var(--clr-gradient-secondary-to)",
     },
+    backgroundImage: {
+      "gradient-primary":
+        "linear-gradient(135deg, var(--clr-gradient-primary-from), var(--clr-gradient-primary-to))",
+      "gradient-secondary":
+        "linear-gradient(135deg, var(--clr-gradient-secondary-from), var(--clr-gradient-secondary-to))",
+    },
+    extend: {},
   },
   plugins: [],
 };

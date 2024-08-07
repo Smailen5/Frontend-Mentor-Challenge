f4560680e4d7d092c8c82b05c7c5ce6c98e70e78
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-07 19:55:37 +0200

    refactor: :construction: aggiunge testo
    aggiunge testo in hero e bozza di codice

--- base-apparel-coming/src/Components/Hero.jsx
+++ base-apparel-coming/src/Components/Hero.jsx
@@ -9,6 +9,19 @@ const Hero = () => {
           className="w-full pt-[5.5rem]"
         />
       </picture>
+
+      <div className="flex items-center justify-center bg-gradient-primary pb-24 pt-14">
+        <section className="h-[310px] w-6/12 text-center">
+          <h2 className="uppercase text-primary font-light text-4.5xl">
+            we&apos;re <span className="text-neutral">coming soon</span>
+          </h2>
+          <p>
+            Hello fellow shoppers! We&apos;re currently building our new fashion
+            store. Add your email below to stay up-to-date with announcements
+            and our launch deals.
+          </p>
+        </section>
+      </div>
     </section>
   );
 };

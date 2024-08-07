5c370c30a533ef5efdab6076b3d1d798359e3f95
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-07 14:58:03 +0200

    style: :iphone: aumenta testo
    aumenta grandezza testo e icone, aumenta margine superiore per tutta la navbar

--- single-page-developer-portfolio/src/components/Navbar.jsx
+++ single-page-developer-portfolio/src/components/Navbar.jsx
@@ -9,10 +9,10 @@ import { useGlobalContext } from "../context";
 const Navbar = () => {
   const { brandLogo } = useGlobalContext();
   return (
-    <nav className="absolute left-0 right-0 top-0 mt-8 flex flex-col items-center justify-center gap-4 md:mx-8 md:flex-row md:justify-between xl:ml-36 xl:mr-40">
-      <header className="text-2xl font-bold md:text-3xl">{brandLogo()}</header>
+    <nav className="absolute left-0 right-0 top-0 mt-8 flex flex-col items-center justify-center gap-4 md:mx-8 md:flex-row md:justify-between xl:ml-36 xl:mr-40 2xl:mt-14">
+      <header className="text-2xl font-bold md:text-3xl 2xl:text-5xl">{brandLogo()}</header>
       {/* icone social */}
-      <div className="flex gap-8 text-xl md:text-2xl">
+      <div className="flex gap-8 text-xl md:text-2xl 2xl:text-5xl">
         <a href="#">
           <SiGithub className="hover:fill-skin-accent" />
         </a>

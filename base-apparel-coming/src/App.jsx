010934373c78d8d71f99d2fc1ba0d0b26933bedb
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-08 17:24:34 +0200

    refactor: :fire: rimuove commenti
    rimuove commenti non piu utili

--- base-apparel-coming/src/App.jsx
+++ base-apparel-coming/src/App.jsx
@@ -3,7 +3,6 @@ import Hero from "./Components/Hero";
 function App() {
   return (
     <>
-      {/* <Header /> */}
       <Hero />
     </>
   );
--- base-apparel-coming/src/Components/Hero.jsx
+++ base-apparel-coming/src/Components/Hero.jsx
@@ -11,7 +11,6 @@ const Hero = () => {
 
         <div className="xl:flex xl:w-full xl:flex-col">
           <LogoBrand className="hidden xl:block" classImg="scale-125" />
-          {/* <Header className="mx-auto w-6/12 scale-125 px-0 xl:block" /> */}
           <div className="flex items-center justify-center px-8 pb-24 pt-14 sm:px-0 xl:h-full xl:px-0 xl:py-10">
             <section className="mx-auto flex h-72 flex-col justify-between text-center sm:w-6/12 xl:h-[25rem] xl:w-[28rem] xl:text-left">
               <h2 className="text-4.5xl font-light uppercase tracking-6 text-primary sm:px-8 xl:px-0 xl:text-6xl">

e54cd84a9a1a9c01bcc35626cd6e029e6c3522b5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-08 18:15:03 +0200

    refactor: :lipstick: aumenta dimensioni
    aumenta dimensioni bottone e sposta di conseguenza icona di errore, aumenta padding input field

--- base-apparel-coming/src/Components/FormComponent.jsx
+++ base-apparel-coming/src/Components/FormComponent.jsx
@@ -32,19 +32,19 @@ function FormComponent() {
               type="email"
               name="email"
               placeholder="Email Address"
-              className="peer relative h-12 w-full rounded-full border border-primary-transparent bg-transparent py-3 pl-6 pr-16 text-neutral outline-none placeholder:text-primary placeholder:opacity-50 invalid:border-secondary focus:invalid:border-2 xl:h-14"
+              className="peer relative h-12 w-full rounded-full border border-primary-transparent bg-transparent py-3 pl-6 pr-28 text-neutral outline-none placeholder:text-primary placeholder:opacity-50 invalid:border-secondary focus:invalid:border-2 xl:h-14 xl:pr-40"
             />
             {errors.email && touched.email && (
               <img
                 src="/images/icon-error.svg"
                 alt="icon error"
-                className="absolute right-20 top-1/2 -translate-y-1/2"
+                className="absolute right-20 top-1/2 -translate-y-1/2 xl:right-32"
               />
             )}
             <button
               type="submit"
               disabled={isSubmitting}
-              className="absolute bottom-0 right-0 flex h-full w-16 items-center justify-center rounded-full bg-gradient-secondary text-2xl text-[#fff] shadow-md shadow-primary peer-invalid:cursor-not-allowed xl:h-full"
+              className="absolute bottom-0 right-0 flex h-full w-16 items-center justify-center rounded-full bg-gradient-secondary text-2xl text-[#fff] shadow-md shadow-primary peer-invalid:cursor-not-allowed xl:w-28"
             >
               <img src="/images/icon-arrow.svg" alt="icon arrow" />
             </button>

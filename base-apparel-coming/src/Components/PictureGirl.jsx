31e94e6b736cac2dbf37f1c7e1d11c19b28b78c9
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-08 17:25:44 +0200

    refactor: :art: riorganizza codice
    riorganizza la formatazzione del codice

--- base-apparel-coming/src/Components/PictureGirl.jsx
+++ base-apparel-coming/src/Components/PictureGirl.jsx
@@ -1,14 +1,14 @@
 function PictureGirl() {
-    return (
-      <picture className="flex-shrink-0">
-        <source media="(min-width: 1280px)" srcSet="/images/hero-desktop.jpg" />
-        <img
-          src="/images/hero-mobile.jpg"
-          alt="hero"
-          className="w-full xl:h-full"
-        />
-      </picture>
-    );
-  }
+  return (
+    <picture className="flex-shrink-0">
+      <source media="(min-width: 1280px)" srcSet="/images/hero-desktop.jpg" />
+      <img
+        src="/images/hero-mobile.jpg"
+        alt="hero"
+        className="w-full xl:h-full"
+      />
+    </picture>
+  );
+}
 
-  export default PictureGirl
+export default PictureGirl;

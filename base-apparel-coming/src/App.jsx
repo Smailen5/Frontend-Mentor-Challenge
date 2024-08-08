545f232c84129b327b6529b978a39d56bbfbdd24
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-08 16:52:17 +0200

    style: :iphone: posiziona logo
    posiziona logo per schermi grandi

--- base-apparel-coming/src/App.jsx
+++ base-apparel-coming/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <Header />
       <Hero>
-        <Header className="mx-auto w-6/12 px-0 xl:block hidden xl:mt-8 xl:pb-0" classImg="scale-125" />
+        <Header className="hidden xl:block" classImg="scale-125" />
       </Hero>
     </>
   );
--- base-apparel-coming/src/Components/Header.jsx
+++ base-apparel-coming/src/Components/Header.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 const Header = ({ className, classImg }) => {
   return (
-    <header className={twMerge("bg-transparent w-full p-8 xl:hidden", className)}>
-      <div>
-        <img src="/images/logo.svg" alt="logo" className={twMerge("w-32", classImg)} />
+    <header
+      className={twMerge("w-full bg-transparent p-8 xl:p-0 xl:pt-20 xl:hidden", className)}
+    >
+      <div className="xl:mx-auto xl:pl-8">
+        <div className="xl:mx-auto xl:w-[28rem]">
+          <img
+            src="/images/logo.svg"
+            alt="logo"
+            className={twMerge("w-32", classImg)}
+          />
+        </div>
       </div>
     </header>
   );

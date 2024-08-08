3aa1c6670523fdeb416b433d38923fb7cb03d8d5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-08 17:53:38 +0200

    refactor: :recycle: sposta messaggio
    sposta messaggio di errore e lo rende absolute per non interferire con la grandezza del div del form

--- base-apparel-coming/src/Components/FormComponent.jsx
+++ base-apparel-coming/src/Components/FormComponent.jsx
@@ -32,27 +32,27 @@ function FormComponent() {
               type="email"
               name="email"
               placeholder="Email Address"
-              className="peer h-12 w-full rounded-full border border-primary-transparent bg-transparent py-3 pl-6 pr-16 text-neutral outline-none placeholder:text-primary placeholder:opacity-50 invalid:border-secondary focus:invalid:border-2 xl:h-14"
-            />
-            <ErrorMessage
-              name="email"
-              component="p"
-              className="mt-1 pl-6 text-left text-secondary"
+              className="peer relative h-12 w-full rounded-full border border-primary-transparent bg-transparent py-3 pl-6 pr-16 text-neutral outline-none placeholder:text-primary placeholder:opacity-50 invalid:border-secondary focus:invalid:border-2 xl:h-14"
             />
             {errors.email && touched.email && (
               <img
                 src="/images/icon-error.svg"
                 alt="icon error"
-                className="absolute right-20 top-1/3 -translate-y-1/2"
+                className="absolute right-20 top-1/2 -translate-y-1/2"
               />
             )}
             <button
               type="submit"
               disabled={isSubmitting}
-              className="absolute bottom-0 right-0 top-0 flex h-full w-16 items-center justify-center rounded-full bg-gradient-secondary text-2xl text-[#fff] shadow-md shadow-primary peer-invalid:cursor-not-allowed xl:h-full"
+              className="absolute bottom-0 right-0 flex h-full w-16 items-center justify-center rounded-full bg-gradient-secondary text-2xl text-[#fff] shadow-md shadow-primary peer-invalid:cursor-not-allowed xl:h-full"
             >
               <img src="/images/icon-arrow.svg" alt="icon arrow" />
             </button>
+            <ErrorMessage
+              name="email"
+              component="p"
+              className="absolute -bottom-8 pl-6 text-secondary"
+            />
           </div>
         </Form>
       )}

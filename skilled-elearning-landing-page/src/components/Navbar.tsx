21bb5bbb45533090bb645477724a0ee1bf4d8d4e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-10 22:50:06 +0200

    refactor: :lipstick: aggiunge stile
    aggiunge stile di base e importa Button

--- skilled-elearning-landing-page/src/components/Navbar.tsx
+++ skilled-elearning-landing-page/src/components/Navbar.tsx
@@ -1,3 +1,13 @@
+import logo from "../assets/logo-dark.svg";
+import { Button } from "./ui/Button";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="p-4">
+      <nav className="flex justify-between items-center">
+        <img src={logo} alt="logo" />
+        <Button />
+      </nav>
+    </header>
+  );
 };

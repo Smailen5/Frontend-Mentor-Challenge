01867686be5803f4cf17a6bef8fff33acb3ad52e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-10 22:10:38 +0200

    refactor: :art: esporta testo
    esporta testo card e hero all'interno di oggetti in data

--- skilled-elearning-landing-page/src/App.tsx
+++ skilled-elearning-landing-page/src/App.tsx
@@ -2,59 +2,6 @@ function App() {
   return (
     <>
       <div>App</div>
-      {/* testo hero */}
-      Maximize skill, minimize budget
-
-      Our modern courses across a range og in-demand
-      skills will give you the knowledge you need to live
-      the life you want.
-
-      {/* testo cards */}
-      Check out our most popular courses!
-      {/* animation */}
-      Animation 
-      Learn the latest animation
-      techniques to create stunning
-      motion design and captivate
-      your audience.
-
-      Get Stared 
-      {/* design */}
-      Design 
-
-      Create beautiful, usable
-      interfaces to help shape the 
-      future of how the web looks.
-
-      Get Started 
-      {/* photography */}
-      Photography 
-
-      Explore critical fundamentals like
-      lighting, composition, and focus
-      to capture exceptional photos.
-
-      Get Started
-      {/* crypto */}
-      Crypto 
-
-      All you need to know to get 
-      started investing in crypto. Go 
-      from beginner to advanced with
-      this 54 hour course.
-
-      Get Started 
-      {/* business */}
-      Business
-
-      A step-by-step playbook to help 
-      you start, scale, and sustain your
-      business without outside
-      investment.
-
-      Get Started
-
-
     </>
   );
 }
---
+++ skilled-elearning-landing-page/src/data/data.ts
@@ -0,0 +1,40 @@
+const heroData = {
+  title: "Maximize skill, minimize budget",
+  subtitle:
+    "Our modern courses across a range og in-demand skills will give you the knowledge you need to live the life you want.",
+};
+
+const dataCards = [
+  { title: "Check out our most popular courses!" },
+
+  {
+    title: "Animation",
+    description:
+      "Learn the latest animation techniques to create stunning motion design and captivate your audience.",
+  },
+
+  {
+    title: "Design",
+    description:
+      "Create beautiful, usable interfaces to help shape the future of how the web looks.",
+  },
+  {
+    title: "Photography",
+    description:
+      "Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.",
+  },
+
+  {
+    title: "Crypto",
+    description:
+      "All you need to know to get  started investing in crypto. Go from beginner to advanced with this 54 hour course.",
+  },
+
+  {
+    title: "Business",
+    description:
+      "A step-by-step playbook to help  you start, scale, and sustain your business without outside investment.",
+  },
+];
+
+export { heroData, dataCards };

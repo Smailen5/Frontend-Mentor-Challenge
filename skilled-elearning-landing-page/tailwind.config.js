336eaaca64b0e34dc5f90bcb4752d4a1f58f5724
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-10 23:49:13 +0200

    refactor: :memo: aggiorna file
    aggiorna file di configurazione ed altri file per permettere il funzionamento di shadcn

--- skilled-elearning-landing-page/tailwind.config.js
+++ skilled-elearning-landing-page/tailwind.config.js
@@ -1,21 +1,67 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        "clr-blue": "var(--primary-clr-blue)",
-        "clr-light-blue": "var(--primary-clr-light-blue)",
-        "clr-pink": "var(--primary-clr-pink)",
-        "clr-light-pink": "var(--primary-clr-light-pink)",
-        "clr-white": "var(--primary-clr-white)",
-        "clr-grey": "var(--primary-clr-grey)",
-      },
-      backgroundImage: {
-        "gradient-pink": "var(--gradient-clr-pink)",
-        "gradient-blue": "var(--gradient-clr-blue)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			'clr-blue': 'var(--primary-clr-blue)',
+  			'clr-light-blue': 'var(--primary-clr-light-blue)',
+  			'clr-pink': 'var(--primary-clr-pink)',
+  			'clr-light-pink': 'var(--primary-clr-light-pink)',
+  			'clr-white': 'var(--primary-clr-white)',
+  			'clr-grey': 'var(--primary-clr-grey)',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		backgroundImage: {
+  			'gradient-pink': 'var(--gradient-clr-pink)',
+  			'gradient-blue': 'var(--gradient-clr-blue)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };

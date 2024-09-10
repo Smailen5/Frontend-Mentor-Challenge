53da76aa201800e6800f58f25b1afe98ebd27ac7
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-10 23:37:36 +0200

    refactor: :lipstick: importa variabili
    importa variabili nel tema di tailwind

--- skilled-elearning-landing-page/tailwind.config.js
+++ skilled-elearning-landing-page/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "clr-blue": "var(--primary-clr-blue)",
+        "clr-light-blue": "var(--primary-clr-light-blue)",
+        "clr-pink": "var(--primary-clr-pink)",
+        "clr-light-pink": "var(--primary-clr-light-pink)",
+        "clr-white": "var(--primary-clr-white)",
+        "clr-grey": "var(--primary-clr-grey)",
+      },
+      backgroundImage: {
+        "gradient-pink": "var(--gradient-clr-pink)",
+        "gradient-blue": "var(--gradient-clr-blue)",
+      },
+    },
   },
   plugins: [],
-}
-
+};

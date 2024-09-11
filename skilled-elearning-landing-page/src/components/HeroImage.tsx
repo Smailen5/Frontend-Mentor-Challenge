ee294d194c8c1224e32658e46986bd26b1d4c8f1
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 22:01:57 +0200

    refactor: :art: aggiunge immagini
    aggiunge immagini per tablet

--- skilled-elearning-landing-page/src/components/HeroImage.tsx
+++ skilled-elearning-landing-page/src/components/HeroImage.tsx
@@ -2,21 +2,41 @@ import imgHeroPng from "../assets/image-hero-mobile.png";
 import imgHeroWebp from "../assets/image-hero-mobile.webp";
 import imgHero2xPng from "../assets/image-hero-mobile@2x.png";
 import imgHero2xWebp from "../assets/image-hero-mobile@2x.webp";
+import imgHeroTabletPng from "../assets/image-hero-tablet.png";
+import imgHeroTabletWebp from "../assets/image-hero-tablet.webp";
+import imgHeroTabletPng2x from "../assets/image-hero-tablet@2x.png";
+import imgHeroTabletWebp2x from "../assets/image-hero-tablet@2x.webp";
 
 export const HeroImage = () => {
   return (
-    <picture className="mt-10">
+    <picture className="h-auto w-full">
+      {/* IMMAGINI DESKTOP */}
+
+      {/* IMMAGINI TABLET */}
       <source
-        srcSet={`${imgHero2xWebp} 2x, ${imgHeroWebp} 1x`}
+        srcSet={`${imgHeroTabletWebp2x} 2x, ${imgHeroTabletWebp} 1x`}
         type="image/webp"
         media="(min-width: 768px)"
       />
       <source
-        srcSet={`${imgHero2xPng} 2x, ${imgHeroPng} 1x`}
+        srcSet={`${imgHeroTabletPng2x} 2x, ${imgHeroTabletPng} 1x`}
         type="image/png"
         media="(min-width: 768px)"
       />
-      <img src={imgHeroWebp} alt="image hero" className="scale-125" />
+      {/* IMMAGINI MOBILE */}
+      <source
+        srcSet={`${imgHero2xWebp} 2x, ${imgHeroWebp} 1x`}
+        type="image/webp"
+      />
+      <source
+        srcSet={`${imgHero2xPng} 2x, ${imgHeroPng} 1x`}
+        type="image/png"
+      />
+      <img
+        src={imgHeroWebp}
+        alt="image hero"
+        className="h-auto w-full scale-125 object-cover md:absolute md:-top-1/2 md:ml-20 md:min-h-[150%] md:min-w-[150%]"
+      />
     </picture>
   );
 };

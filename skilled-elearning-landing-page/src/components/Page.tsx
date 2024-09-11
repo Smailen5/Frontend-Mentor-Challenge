a67111aa99773103823dd760376655f898c4da61
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 20:01:29 +0200

    refactor: :art: aggiunge parametro
    aggiunge isOpacity per gestire i casi dove serve avere il background

--- skilled-elearning-landing-page/src/components/Page.tsx
+++ skilled-elearning-landing-page/src/components/Page.tsx
@@ -1,6 +1,19 @@
 type PageProps = {
   children: React.ReactNode;
+  isOpacity?: boolean;
 };
-export const Page: React.FC<PageProps> = ({ children }) => {
-  return <section className="px-4 py-10">{children}</section>;
+export const Page: React.FC<PageProps> = ({ children, isOpacity }) => {
+  return (
+    <section className="px-4 py-20 bg-opacity-5 relative">
+      {isOpacity && (
+        <>
+          <div className="absolute inset-0 bg-gradient-to-t from-clr-light-blue to-clr-white">
+            <div className="absolute inset-0 bg-clr-white opacity-85" />
+          </div>
+        </>
+      )}
+
+      <div className="relative z-10">{children}</div>
+    </section>
+  );
 };

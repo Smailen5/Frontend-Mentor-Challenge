80ac8702c3fe0581eda3483f930a4f13203f4e10
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 18:13:24 +0200

    refactor: :art: aggiunge importazioni
    aggiunge le importazioni per i nuovi componenti

--- skilled-elearning-landing-page/src/components/index.tsx
+++ skilled-elearning-landing-page/src/components/index.tsx
@@ -2,5 +2,7 @@ import { Card } from "./Card";
 import { Footer } from "./Footer";
 import { Hero } from "./Hero";
 import { Navbar } from "./Navbar";
+import { Page } from "./Page";
+import { HeroImage } from "./HeroImage";
 
-export { Card, Footer, Hero, Navbar };
+export { Card, Footer, Hero, Navbar, Page, HeroImage };

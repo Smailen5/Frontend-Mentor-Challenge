59b3b4e1a613fed9394502b25026f1372522a70b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 17:59:51 +0200

    refactor: :art: crea componente
    crea il componente HeroImage dal momento che diventa sempre piu grande meglio gestirlo separatamente

---
+++ skilled-elearning-landing-page/src/components/HeroImage.tsx
@@ -0,0 +1,22 @@
+import imgHeroPng from "../assets/image-hero-mobile.png";
+import imgHeroWebp from "../assets/image-hero-mobile.webp";
+import imgHero2xPng from "../assets/image-hero-mobile@2x.png";
+import imgHero2xWebp from "../assets/image-hero-mobile@2x.webp";
+
+export const HeroImage = () => {
+  return (
+    <picture>
+      <source
+        srcSet={`${imgHero2xWebp} 2x, ${imgHeroWebp} 1x`}
+        type="image/webp"
+        media="(min-width: 768px)"
+      />
+      <source
+        srcSet={`${imgHero2xPng} 2x, ${imgHeroPng} 1x`}
+        type="image/png"
+        media="(min-width: 768px)"
+      />
+      <img src={imgHeroWebp} alt="image hero" />
+    </picture>
+  );
+};

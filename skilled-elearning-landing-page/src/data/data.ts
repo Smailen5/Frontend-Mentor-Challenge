f64c97e72e3a45899157492c8bb76a898a6dafaa
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 19:22:13 +0200

    refactor: :art: aggiunge chiave
    aggiunge nuova chiave-valore per le immagini

--- skilled-elearning-landing-page/src/data/data.ts
+++ skilled-elearning-landing-page/src/data/data.ts
@@ -8,29 +8,34 @@ const dataCards = [
   { title: "Check out our most popular courses!" },
 
   {
+    image: "animation",
     title: "Animation",
     description:
       "Learn the latest animation techniques to create stunning motion design and captivate your audience.",
   },
 
   {
+    image: "design",
     title: "Design",
     description:
       "Create beautiful, usable interfaces to help shape the future of how the web looks.",
   },
   {
+    image: "photography",
     title: "Photography",
     description:
       "Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.",
   },
 
   {
+    image: "crypto",
     title: "Crypto",
     description:
       "All you need to know to get  started investing in crypto. Go from beginner to advanced with this 54 hour course.",
   },
 
   {
+    image: "business",
     title: "Business",
     description:
       "A step-by-step playbook to help  you start, scale, and sustain your business without outside investment.",

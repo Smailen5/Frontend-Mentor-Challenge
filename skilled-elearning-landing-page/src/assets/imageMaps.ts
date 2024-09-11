a0466adc57c7a0f91499eae532124dd613d61257
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 19:21:43 +0200

    refactor: :art: crea indice
    crea indice per le icone

---
+++ skilled-elearning-landing-page/src/assets/imageMaps.ts
@@ -0,0 +1,13 @@
+import animation from "./icon-animation.svg";
+import design from "./icon-design.svg";
+import crypto from "./icon-crypto.svg";
+import photography from "./icon-photography.svg";
+import business from "./icon-business.svg";
+
+export const imageMap: { [key: string]: string } = {
+  animation,
+  design,
+  crypto,
+  photography,
+  business,
+};

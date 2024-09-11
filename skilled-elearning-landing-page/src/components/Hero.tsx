6280c1e5181d8cb5e1752ad1dc824e13581a7a99
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 17:24:42 +0200

    refactor: :lipstick: aggiorna stile
    aggiorna lo stile del testo

--- skilled-elearning-landing-page/src/components/Hero.tsx
+++ skilled-elearning-landing-page/src/components/Hero.tsx
@@ -5,9 +5,11 @@ import { Button } from "./ui/Button";
 export const Hero = () => {
   return (
     <Page>
-      <h1>{heroData.title}</h1>
-      <p>{heroData.subtitle}</p>
-      <Button size={"lg"}>Get Started</Button>
+      <div className="flex flex-col gap-8 items-start">
+        <h1 className="text-[2.5rem] font-extrabold text-clr-blue">{heroData.title}</h1>
+        <p className="text-clr-grey text-xl">{heroData.subtitle}</p>
+        <Button size={"lg"}>Get Started</Button>
+      </div>
     </Page>
   );
 };

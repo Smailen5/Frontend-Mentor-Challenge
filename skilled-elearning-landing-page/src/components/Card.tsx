17d8917533d21b22f5394ec727255d384251e821
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 19:24:34 +0200

    refactor: :art: crea componenti
    crea Cards con map di Card, aggiunge stile per entrambi

--- skilled-elearning-landing-page/src/components/Card.tsx
+++ skilled-elearning-landing-page/src/components/Card.tsx
@@ -1,3 +1,28 @@
-export const Card = () => {
-  return <div>Card</div>;
+import { Button } from "./ui/Button";
+import { CardImage } from "./";
+
+type CardProps = {
+  title: string;
+  description?: string;
+  image?: string;
+};
+
+export const Card: React.FC<CardProps> = ({ title, description, image }) => {
+  // se la descrizione non esiste torna una card diversa
+  if (!description) {
+    return (
+      <article className="bg-gradient-pink p-8 rounded-2xl ">
+        <h3 className="text-clr-white font-bold text-2xl">{title}</h3>
+      </article>
+    );
+  }
+
+  return (
+    <article className="bg-clr-white p-8 pt-12 rounded-2xl space-y-4 relative">
+      <CardImage image={image} />
+      <h3 className="text-clr-blue font-bold text-2xl">{title}</h3>
+      <p className="text-clr-grey font-semibold">{description}</p>
+      <Button variant={"ghost"}>Get Started</Button>
+    </article>
+  );
 };
--- skilled-elearning-landing-page/src/components/Cards.tsx
+++ skilled-elearning-landing-page/src/components/Cards.tsx
@@ -1,3 +1,15 @@
+import { dataCards } from "../data/data";
+import { Card } from "./Card";
+import { Page } from "./Page";
+
 export const Cards = () => {
-  return <div>Cards</div>;
+  return (
+    <Page>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {dataCards.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
+      </section>
+    </Page>
+  );
 };

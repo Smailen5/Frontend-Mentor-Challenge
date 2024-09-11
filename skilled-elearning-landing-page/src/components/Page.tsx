35992747b3bea5c4ea13182ab834637fc965366f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 17:24:20 +0200

    refactor: :lipstick: imposta padding
    imposta padding laterale e sopra e sotto per tutte le pagine

--- skilled-elearning-landing-page/src/components/Page.tsx
+++ skilled-elearning-landing-page/src/components/Page.tsx
@@ -2,5 +2,5 @@ type PageProps = {
   children: React.ReactNode;
 };
 export const Page: React.FC<PageProps> = ({ children }) => {
-  return <section>{children}</section>;
+  return <section className="px-4 py-10">{children}</section>;
 };

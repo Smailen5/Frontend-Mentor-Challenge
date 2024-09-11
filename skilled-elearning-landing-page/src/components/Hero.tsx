593bccbc5ccf08916fbf7b1baa163e5dbf39d9dd
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-11 17:08:07 +0200

    refactor: :art: importa componente
    importa Page in App

--- skilled-elearning-landing-page/src/components/Hero.tsx
+++ skilled-elearning-landing-page/src/components/Hero.tsx
@@ -1,12 +1,13 @@
 import { heroData } from "../data/data";
+import { Page } from "./Page";
 import { Button } from "./ui/Button";
 
 export const Hero = () => {
   return (
-    <section>
+    <Page>
       <h1>{heroData.title}</h1>
       <p>{heroData.subtitle}</p>
       <Button size={"lg"}>Get Started</Button>
-    </section>
+    </Page>
   );
 };

1bfe2eb73710eef7c039d08dd380e54ddb52db47
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-12 14:58:16 +0200

    refactor: :art: ordina importazioni
    cambia oridine importazioni

--- skilled-elearning-landing-page/src/components/HeroImage.tsx
+++ skilled-elearning-landing-page/src/components/HeroImage.tsx
@@ -1,3 +1,7 @@
+import imgHeroDesktopPng from "../assets/image-hero-desktop.png";
+import imgHeroDesktopWebp from "../assets/image-hero-desktop.webp";
+import imgHeroDesktopPng2x from "../assets/image-hero-desktop@2x.png";
+import imgHeroDesktopWebp2x from "../assets/image-hero-desktop@2x.webp";
 import imgHeroPng from "../assets/image-hero-mobile.png";
 import imgHeroWebp from "../assets/image-hero-mobile.webp";
 import imgHero2xPng from "../assets/image-hero-mobile@2x.png";
@@ -6,50 +10,6 @@ import imgHeroTabletPng from "../assets/image-hero-tablet.png";
 import imgHeroTabletWebp from "../assets/image-hero-tablet.webp";
 import imgHeroTabletPng2x from "../assets/image-hero-tablet@2x.png";
 import imgHeroTabletWebp2x from "../assets/image-hero-tablet@2x.webp";
-import imgHeroDesktopPng from "../assets/image-hero-desktop.png";
-import imgHeroDesktopWebp from "../assets/image-hero-desktop.webp";
-import imgHeroDesktopPng2x from "../assets/image-hero-desktop@2x.png";
-import imgHeroDesktopWebp2x from "../assets/image-hero-desktop@2x.webp";
-
-// export const HeroImage = () => {
-//   return (
-//     <picture className="h-auto w-full md:absolute md:right-[-20px] md:top-[-180px]">
-//       {/* IMMAGINI DESKTOP */}
-
-//       {/* IMMAGINI TABLET */}
-//       <source
-//         srcSet={`${imgHeroTabletWebp2x} 2x, ${imgHeroTabletWebp} 1x`}
-//         type="image/webp"
-//         media="(min-width: 768px)"
-//       />
-//       <source
-//         srcSet={`${imgHeroTabletPng2x} 2x, ${imgHeroTabletPng} 1x`}
-//         type="image/png"
-//         media="(min-width: 768px)"
-//       />
-//       {/* IMMAGINI MOBILE */}
-//       <source
-//         srcSet={`${imgHero2xWebp} 2x, ${imgHeroWebp} 1x`}
-//         type="image/webp"
-//       />
-//       <source
-//         srcSet={`${imgHero2xPng} 2x, ${imgHeroPng} 1x`}
-//         type="image/png"
-//       />
-//       <img
-//         src={imgHeroWebp}
-//         alt="image hero"
-//         className="md:transform-origin-top-right h-auto w-full scale-125 object-cover md:absolute md:ml-20 md:min-h-[150%] md:min-w-[150%] md:translate-y-[-10%] md:transform"
-//       />
-
-//       {/* <img
-//         src={imgHeroWebp}
-//         alt="image hero"
-//         className="h-auto w-full scale-125 object-cover md:absolute md:ml-20 md:min-h-[150%] md:min-w-[150%]"
-//       /> */}
-//     </picture>
-//   );
-// };
 
 export const HeroImage = () => {
   return (

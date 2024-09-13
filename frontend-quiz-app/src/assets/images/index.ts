4291ebeb66e124d1722274caad488b4f455516e6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-13 18:47:33 +0200

    refactor: :technologist: crea indice
    crea indice per le icone

---
+++ frontend-quiz-app/src/assets/images/index.ts
@@ -0,0 +1,7 @@
+// import iconAccessibility from "./icon-accessibility.svg";
+import iconCss from "./icon-css.svg";
+import iconHtml from "./icon-html.svg";
+import iconJavascript from "./icon-js.svg";
+import iconAccessibility from "./icon-accessibility.svg";
+
+export const iconMaps:{ [ key:string ]:string } = { iconCss, iconHtml, iconJavascript, iconAccessibility }

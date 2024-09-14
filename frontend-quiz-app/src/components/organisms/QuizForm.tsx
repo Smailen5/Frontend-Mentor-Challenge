049c9a8d00e8b9134c7aab5563b2336c64b80396
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-14 20:40:55 +0200

    feat: :art: esporta componenti
    esporta il componente che gestisce i quiz e crea un useQuiz che gestisce quale quiz mostrare

--- frontend-quiz-app/src/components/organisms/QuizForm.tsx
+++ frontend-quiz-app/src/components/organisms/QuizForm.tsx
@@ -1,3 +1,47 @@
-export const QuizForm = () => {
-  return <div>QuizForm</div>;
+import { useState } from "react";
+import { ButtonAnswer } from "../index";
+
+type Questions = {
+  question: string;
+  options: string[];
+  answer: string;
+};
+type QuizFormProps = {
+  questions: Questions[];
+};
+
+export const QuizForm: React.FC<QuizFormProps> = ({ questions }) => {
+  // stato per l'indice della domanda corrente
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const goToNextQuestion = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      // gestisci la fine del test qui, manda il componente ScorePage
+      alert("test complete");
+    }
+  };
+
+  // ottiene la domanda corrente
+  const question = questions[currentQuestionIndex];
+
+  return (
+    <>
+      <div>
+        question {currentQuestionIndex + 1} di {questions.length}
+      </div>
+      <div>
+        <h2>{question.question}</h2>
+        <div>barra di quante domande sono state fatte e quante ne mancano</div>
+        <section className="flex flex-col gap-4">
+          {question.options.map((option, optionIndex) => (
+            <ButtonAnswer key={optionIndex} option={option} />
+          ))}
+          <button onClick={goToNextQuestion}>vai avanti</button>
+        </section>
+      </div>
+      ;
+    </>
+  );
 };

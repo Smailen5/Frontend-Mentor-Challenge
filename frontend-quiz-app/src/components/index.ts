e5bff79710710c666ee4de639cb303035e5b5c52
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-14 17:46:09 +0200

    feat: :truck: aggiunge esportazione
    aggiunge nuovo componente alle esportazioni

--- frontend-quiz-app/src/components/index.ts
+++ frontend-quiz-app/src/components/index.ts
@@ -5,10 +5,11 @@ export { Button } from "./atoms/Button";
 export { ToggleButton } from "./atoms/ToggleButton";
 
 // Molecules
-export { Hero } from "./molecules/Hero";
 export { Navbar } from "./molecules/Navbar";
+export { ButtonsSection } from "./molecules/ButtonsSection"
 
 // Organisms
+export { Hero } from "./organisms/Hero";
 export { Header } from "./organisms/Header";
 export { QuizForm } from "./organisms/QuizForm";
 export { Score } from "./organisms/Score";

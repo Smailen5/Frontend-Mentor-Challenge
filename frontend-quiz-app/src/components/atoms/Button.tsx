e7ee428c724fbae67b200f80639393736c4cdefd
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-14 18:20:13 +0200

    style: :lipstick: aggiorna stile
    aggiunge larghezza bottone

--- frontend-quiz-app/src/components/atoms/Button.tsx
+++ frontend-quiz-app/src/components/atoms/Button.tsx
@@ -6,11 +6,11 @@ type ButtonProps = {
 };
 
 export const Button: React.FC<ButtonProps> = ({ children, icon }) => {
-    const iconSrc = icon? iconMaps[icon] : undefined;
-    console.log(iconSrc);
-    
+  const iconSrc = icon ? iconMaps[icon] : undefined;
+  console.log(iconSrc);
+
   return (
-    <button className="bg-slate-400 rounded-lg flex items-center gap-4 p-4">
+    <button className="flex w-full items-center gap-4 rounded-lg bg-slate-400 p-4">
       <img src={iconSrc} alt="icona" />
       {children}
     </button>

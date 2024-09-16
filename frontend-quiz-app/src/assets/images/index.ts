58a4430e5c9f88511f7742210c0cd4ee9978e110
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-17 01:09:57 +0200

    refactor: :recycle: rinomina le icone come componenti
    rinomino le icone con la lettera maiuscola iniziale per farle riconoscere come componenti react

--- frontend-quiz-app/src/assets/images/index.ts
+++ frontend-quiz-app/src/assets/images/index.ts
@@ -1,7 +1,15 @@
-// import iconAccessibility from "./icon-accessibility.svg";
-import iconCss from "./icon-css.svg";
-import iconHtml from "./icon-html.svg";
-import iconJavascript from "./icon-js.svg";
-import iconAccessibility from "./icon-accessibility.svg";
+import IconAccessibility from "./icon-accessibility.svg?react";
+import IconCss from "./icon-css.svg?react";
+import IconHtml from "./icon-html.svg?react";
+import IconJavascript from "./icon-js.svg?react";
 
-export const iconMaps:{ [ key:string ]:string } = { iconCss, iconHtml, iconJavascript, iconAccessibility }
+type IconComponents = {
+  [key: string]: React.FC<React.SVGProps<SVGSVGElement>>;
+};
+
+export const iconMaps:IconComponents = {
+  IconCss,
+  IconHtml,
+  IconJavascript,
+  IconAccessibility,
+};

252b6079e64162365a441e91e7738c8b894a3917
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-16 21:23:41 +0200

    fix: :fire: rimuovi codice inutile per IconQuiz
    rimuovo codice utilizzato in precedenza per creare IconQuiz

--- frontend-quiz-app/src/components/organisms/Score.tsx
+++ frontend-quiz-app/src/components/organisms/Score.tsx
@@ -1,4 +1,3 @@
-import { iconMaps } from "../../assets/images/index";
 import IconQuiz from "../molecules/IconQuiz";
 
 type ScoreProps = {
@@ -13,23 +12,21 @@ export const Score: React.FC<ScoreProps> = ({
   correctAnswers,
   questionLength,
 }) => {
-  const iconSrc = iconMaps[icon];
-  console.log(iconSrc);
-
   return (
     <section className="flex flex-col gap-6 pt-4">
       <h2 className="text-4xl font-light">
         Quiz completed <br />
         <span className="font-bold">You scored...</span>
       </h2>
+
       <div className="space-y-2">
         <div className="flex w-full flex-col items-center gap-4 rounded-md bg-red-200 p-4">
           <IconQuiz title={title} icon={icon} />
-          <h3>{title}</h3>
+          <span className="text-7xl font-bold">{correctAnswers}</span>
+          <span>of out {questionLength}</span>
         </div>
-        <span className="text-7xl font-bold">{correctAnswers}</span>
-        <span>of out {questionLength}</span>
       </div>
+
       <button className="w-full rounded-md bg-violet-500 p-2 text-white">
         Play Again
       </button>

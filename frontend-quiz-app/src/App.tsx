64f4da563dde807c4575772b6c54bb15745a1376
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-16 15:54:19 +0200

    refactor: :fire: rimuovi codice sbagliato di useLocation
    elimina il codice di useLocatione e tutti gli if per gestire il titolo della navbar

--- frontend-quiz-app/src/App.tsx
+++ frontend-quiz-app/src/App.tsx
@@ -1,29 +1,10 @@
-import { RouterProvider, useLocation } from "react-router-dom";
-import { Navbar, Page } from "./components";
+import { RouterProvider } from "react-router-dom";
+import { Page } from "./components";
 import { router } from "./routes/routes";
-import { useEffect, useState } from "react";
 
 function App() {
-  const location = useLocation()
-  const [quizTitle, setQuizTitle] = useState("")
-
-  useEffect(()=>{
-    if (location.pathname.includes('/html')) {
-      setQuizTitle('HTML quiz')
-    } else if(location.pathname.includes('/css')){
-      setQuizTitle('CSS quiz')
-    } else if(location.pathname.includes('/javascript')){
-      setQuizTitle('javascript quiz')
-    } else if(location.pathname.includes('/accessibility')){
-      setQuizTitle('accessibility quiz')
-    } else{
-      setQuizTitle('')
-    }
-  },[location])
-  
   return (
     <>
-      <Navbar quizzes={{title: quizTitle}} />
       <Page>
         <RouterProvider router={router} />
       </Page>

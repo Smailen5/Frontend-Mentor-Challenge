cc3dabd2ffccb956e94063e13f5347a5f8a86c77
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-16 21:20:58 +0200

    feat: :poop: crea il componente Score
    creo il componente Score con una base di stile e importando il componente IconQuiz

--- frontend-quiz-app/src/components/organisms/Score.tsx
+++ frontend-quiz-app/src/components/organisms/Score.tsx
@@ -1,7 +1,38 @@
+import { iconMaps } from "../../assets/images/index";
+import IconQuiz from "../molecules/IconQuiz";
+
 type ScoreProps = {
+  title: string;
+  icon: string;
   correctAnswers?: number;
-  questionLength?:number;
-}
-export const Score:React.FC<ScoreProps> = ({correctAnswers, questionLength}) => {
-  return <div>le risposte corrette sono {correctAnswers} di {questionLength} </div>;
+  questionLength?: number;
+};
+export const Score: React.FC<ScoreProps> = ({
+  title,
+  icon,
+  correctAnswers,
+  questionLength,
+}) => {
+  const iconSrc = iconMaps[icon];
+  console.log(iconSrc);
+
+  return (
+    <section className="flex flex-col gap-6 pt-4">
+      <h2 className="text-4xl font-light">
+        Quiz completed <br />
+        <span className="font-bold">You scored...</span>
+      </h2>
+      <div className="space-y-2">
+        <div className="flex w-full flex-col items-center gap-4 rounded-md bg-red-200 p-4">
+          <IconQuiz title={title} icon={icon} />
+          <h3>{title}</h3>
+        </div>
+        <span className="text-7xl font-bold">{correctAnswers}</span>
+        <span>of out {questionLength}</span>
+      </div>
+      <button className="w-full rounded-md bg-violet-500 p-2 text-white">
+        Play Again
+      </button>
+    </section>
+  );
 };

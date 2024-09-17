8ac9cad283d7b553ff21d2e908ac9ea015c2e287
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-17 16:30:52 +0200

    refactor: :truck: aggiungi icone nel indice
    aggiungo le icone per la dark mode nel indice di icone per una migliore importazione

--- frontend-quiz-app/src/assets/images/index.ts
+++ frontend-quiz-app/src/assets/images/index.ts
@@ -2,14 +2,38 @@ import IconAccessibility from "./icon-accessibility.svg?react";
 import IconCss from "./icon-css.svg?react";
 import IconHtml from "./icon-html.svg?react";
 import IconJavascript from "./icon-js.svg?react";
+import IconMoonDark from "./icon-moon-dark.svg?react";
+import IconMoonLight from "./icon-moon-light.svg?react";
+import IconSunDark from "./icon-sun-dark.svg?react";
+import IconSunLight from "./icon-sun-light.svg?react";
 
 type IconComponents = {
   [key: string]: React.FC<React.SVGProps<SVGSVGElement>>;
 };
 
-export const iconMaps:IconComponents = {
+type IconDarkMode = {
+  light: {
+    [key: string]: React.FC<React.SVGProps<SVGSVGElement>>;
+  };
+  dark: {
+    [key: string]: React.FC<React.SVGProps<SVGSVGElement>>;
+  };
+};
+
+export const iconMaps: IconComponents = {
   IconCss,
   IconHtml,
   IconJavascript,
   IconAccessibility,
+};
+
+export const iconDarkMode: IconDarkMode = {
+  light: {
+    IconSun: IconSunLight,
+    IconMoon: IconMoonLight,
+  },
+  dark: {
+    IconSun: IconSunDark,
+    IconMoon: IconMoonDark,
+  },
 };

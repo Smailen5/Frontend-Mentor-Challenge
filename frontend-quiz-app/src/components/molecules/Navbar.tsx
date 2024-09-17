5fb9516960bf9fb908d77d6639bfe815d119f8f7
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-17 16:43:15 +0200

    style: :lipstick: aggiorna posizonamento navbar nei quiz
    allineao i componenti al centro della navbar nei quiz

--- frontend-quiz-app/src/components/molecules/Navbar.tsx
+++ frontend-quiz-app/src/components/molecules/Navbar.tsx
@@ -8,14 +8,14 @@ export const Navbar: React.FC<NavbarProps> = ({ quizzes }) => {
   // navbar nelle pagine quiz
   if (quizzes) {
     return (
-      <header className="flex justify-between dark:bg-slate-400">
+      <header className="flex items-center justify-between">
         <IconQuiz title={quizzes.title} icon={quizzes.icon} />
         <ToggleButton />
       </header>
     );
   }
   return (
-    <header className="mx-auto flex max-w-screen-lg justify-end py-2 ">
+    <header className="mx-auto flex max-w-screen-lg justify-end py-2">
       <nav className="">
         <ToggleButton />
       </nav>

dbc7aa934343968caaf5350d3f6e256c27db61a6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-17 14:59:19 +0200

    style: :lipstick: aggiungi icone di sfondo e colore
    aggiungo le svg di sfondo e il colore di sfondo /n il colore non mi sembra uguale forse e colpa di un effetto ottico

--- frontend-quiz-app/src/components/atoms/Page.tsx
+++ frontend-quiz-app/src/components/atoms/Page.tsx
@@ -1,6 +1,50 @@
+import mobileLight from "../../assets/images/pattern-background-mobile-light.svg";
+import tabletLight from "../../assets/images/pattern-background-tablet-light.svg";
+import desktopLight from "../../assets/images/pattern-background-desktop-light.svg";
+import mobileDark from "../../assets/images/pattern-background-mobile-dark.svg";
+import tabletDark from "../../assets/images/pattern-background-tablet-dark.svg";
+import desktopDark from "../../assets/images/pattern-background-desktop-dark.svg";
+import { useDarkModeContext } from "../../utils/darkModeContext";
+
 type PageProps = {
   children: React.ReactNode;
 };
 export const Page: React.FC<PageProps> = ({ children }) => {
-  return <section className="mx-auto p-4 dark:bg-black">{children}</section>;
+  const { darkMode } = useDarkModeContext();
+  return (
+    <>
+      <div className="relative min-h-screen">
+        {/* colore di sfondo */}
+        <div className="absolute inset-0 z-0 bg-gray-100 dark:bg-gray-900" />
+        <picture className="absolute z-0">
+          {/* Tema chiaro/scuro - desktop */}
+          <source
+            srcSet={darkMode ? desktopDark : desktopLight}
+            media="(min-width: 1024px)"
+            className="block dark:hidden"
+          />
+          {/* Tema chiaro/scuro - tablet */}
+          <source
+            srcSet={darkMode ? tabletDark : tabletLight}
+            media="(min-width: 768px)"
+            className="block dark:hidden"
+          />
+          {/* Tema chiaro/scuro - mobile */}
+          <source
+            srcSet={darkMode ? mobileDark : mobileLight}
+            media="(max-width: 767px)"
+            className="block dark:hidden"
+          />
+
+          {/* Fallback per browser senza supporto <picture> */}
+          <img
+            src={darkMode ? mobileDark : mobileLight}
+            alt="Background pattern"
+            className="relative z-0"
+          />
+        </picture>
+        <section className="relative z-10 mx-auto p-4">{children}</section>
+      </div>
+    </>
+  );
 };

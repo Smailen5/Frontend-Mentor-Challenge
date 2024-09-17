2155232bdf367ef22b5473ba4fe160ef5e90cb11
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-17 16:06:45 +0200

    refactor: :recycle: aggiungi il background alle icone
    ristruttura il componente per aggiungere il background delle icone e cambia lo stile per addattarlo

--- frontend-quiz-app/src/components/molecules/IconQuiz.tsx
+++ frontend-quiz-app/src/components/molecules/IconQuiz.tsx
@@ -1,20 +1,32 @@
 import { iconMaps } from "../../assets/images/";
 
 type IconQuizProps = {
-  title?: string;
+  title: string;
   icon: keyof typeof iconMaps;
+};
+
+// Devo inserire manualmente i colori, al momento non riesco a recuperare il fill degli svg
+const iconsBackground: { [key: string]: string } = {
+  IconCss: "#E0FDEF",
+  IconHtml: "#FFF1E9",
+  IconJavascript: "#EBF0FF",
+  IconAccessibility: "#F6E7FF",
 };
 
 const IconQuiz: React.FC<IconQuizProps> = ({ title, icon }) => {
   const IconComponent = iconMaps[icon];
-  console.log(icon);
+  const iconBackground = iconsBackground[icon] || "#ffffff";
 
   return (
     <>
       {IconComponent ? (
-        <div className="flex items-center gap-2">
-          <IconComponent className="h-6 w-6" aria-label={`${title} icon`} />
-          {title && <h2>{title}</h2>}
+        <div className="flex items-center gap-4">
+          <IconComponent
+            style={{ background: iconBackground }}
+            className="size-11 rounded-md p-2"
+            aria-label={`${title} icon`}
+          />
+          {title && <h2 className="text-xl font-bold">{title}</h2>}
         </div>
       ) : (
         <span>Icon not found</span>

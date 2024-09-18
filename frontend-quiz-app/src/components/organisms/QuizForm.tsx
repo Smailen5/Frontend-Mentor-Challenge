f9b49c510e26e4f2e42d1f3307aef1c845148779
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-18 18:06:56 +0200

    refactor: :fire: pulisci il componente dal codice commentato
    elimino tutto il codice commentato che non serve piu

--- frontend-quiz-app/src/components/organisms/QuizForm.tsx
+++ frontend-quiz-app/src/components/organisms/QuizForm.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-// import { ButtonAnswer, Score } from "../index";
-// import { IconsAnswer } from "../molecules/IconsAnswer";
+import { handleNextQuestion, handleSubmitAnswer } from "../../utils/quizUtils";
 import { Score } from "../index";
 import { QuizAnswer } from "../molecules/QuizAnswer";
-import { handleSubmitAnswer, handleNextQuestion } from "../../utils/quizUtils";
 
 type Questions = {
   question: string;
@@ -57,32 +55,6 @@ export const QuizForm: React.FC<QuizFormProps> = ({
       setIsQuizCompleted,
     );
   };
-  // const handleSubmitAnswer = () => {
-  //   // se non selezioni niente
-  //   if (selectedAnswer === null) {
-  //     setNoSelectedAnswer(true);
-  //     return;
-  //   }
-  //   // controlla che la risposta sia corretta
-  //   if (selectedAnswer === question.answer) {
-  //     setCorrectAnswers(correctAnswers + 1);
-  //   }
-  //   setNoSelectedAnswer(false);
-  //   setIsAnswerSubmitted(true); // l'utente ha inviato la risposta
-  // };
-
-  // const handleNextQuestion = () => {
-  //   if (currentQuestionIndex < questions.length - 1) {
-  //     setCurrentQuestionIndex(currentQuestionIndex + 1);
-
-  //     setSelectedAnswer(null);
-
-  //     setIsAnswerSubmitted(false);
-  //   } else {
-  //     // gestisci la fine del test qui, manda il componente ScorePage
-  //     setIsQuizCompleted(true);
-  //   }
-  // };
 
   if (isQuizCompleted) {
     return (
@@ -113,48 +85,6 @@ export const QuizForm: React.FC<QuizFormProps> = ({
           selectedAnswer={selectedAnswer}
           setSelectedAnswer={setSelectedAnswer}
         />
-        {/* {question.options.map((option, optionIndex) => {
-          let buttonStyle = "";
-          let indexStyle = "";
-          let showCorrectIcon = false;
-          let showErrorIcon = false;
-          // se la risposta e stata inviata
-          if (isAnswerSubmitted) {
-            console.log(isAnswerSubmitted);
-
-            if (option === question.answer) {
-              console.log(option === question.answer);
-              // stile per la risposta corretta
-              buttonStyle = "border-2 border-green-500";
-              indexStyle = "bg-green-500 text-white";
-              showCorrectIcon = true;
-            } else if (
-              option === selectedAnswer &&
-              option !== question.answer
-            ) {
-              // stile per la risposta errata
-              buttonStyle = "border-2 border-red-500";
-              indexStyle = "bg-red-500 text-white";
-              showErrorIcon = true;
-            }
-          }
-          return (
-            <ButtonAnswer
-              key={optionIndex}
-              optionIndex={optionIndex}
-              option={option}
-              onClick={() => setSelectedAnswer(option)}
-              isSelected={selectedAnswer === option}
-              classButton={buttonStyle}
-              classIndex={indexStyle}
-            >
-              <IconsAnswer
-                showCorrectIcon={showCorrectIcon}
-                showErrorIcon={showErrorIcon}
-              />
-            </ButtonAnswer>
-          );
-        })} */}
         {!isAnswerSubmitted ? (
           <button
             onClick={handleSubmit}

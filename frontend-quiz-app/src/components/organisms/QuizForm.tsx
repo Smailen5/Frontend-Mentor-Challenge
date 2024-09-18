a2e55f8f00c2a952261ddf7d1ae515c35a700d4a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-18 18:24:52 +0200

    refactor: :fire: elimina la sezione commentata

--- frontend-quiz-app/src/components/organisms/QuizForm.tsx
+++ frontend-quiz-app/src/components/organisms/QuizForm.tsx
@@ -92,24 +92,6 @@ export const QuizForm: React.FC<QuizFormProps> = ({
           handleNext={handleNext}
           noSelectedAnswer={noSelectedAnswer}
         />
-        {/* {!isAnswerSubmitted ? (
-          <button
-            onClick={handleSubmit}
-            className="rounded-xl bg-purple-600 p-4 font-semibold tracking-wider text-white"
-          >
-            Submit Answer
-          </button>
-        ) : (
-          <button
-            onClick={handleNext}
-            className="rounded-xl bg-purple-600 p-4 font-semibold tracking-wider text-white"
-          >
-            Next Question
-          </button>
-        )}
-        {noSelectedAnswer && (
-          <p className="text-center text-red-500">Please select an answer</p>
-        )} */}
       </section>
     </>
   );

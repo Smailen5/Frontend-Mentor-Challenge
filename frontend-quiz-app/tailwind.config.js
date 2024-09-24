0848d1c2e50ff2787573af5c3f323f8b1a05d0b6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-24 15:23:50 +0200

    style: :memo: aggiungi colori personalizzati
    aggiungo colori personalizzati del progetto, importo i colori nel file di configurazione di tailwind

--- frontend-quiz-app/tailwind.config.js
+++ frontend-quiz-app/tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      pureWhite: "hsl(var(--clr-pureWhite))",
+      lightGrey: "hsl(var(--clr-lightGrey ))",
+      lightBluish: "hsl(var(--clr-lightBluish ))",
+      greyNavy: "hsl(var(--clr-greyNavy ))",
+      navy: "hsl(var(--clr-navy ))",
+      darkNavy: "hsl(var(--clr-darkNavy ))",
+      purple: "hsl(var(--clr-purple ))",
+      green: "hsl(var(--clr-green ))",
+      red: "hsl(var(--clr-red ))",
+    },
     extend: {
       fontFamily: {
         rubik: ["Rubik", "sans-serif"],

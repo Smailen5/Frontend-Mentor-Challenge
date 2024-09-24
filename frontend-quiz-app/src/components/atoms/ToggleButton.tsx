3069d4972e484defef1ca59d5d1140894bead2b9
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-24 16:40:50 +0200

    style: :iphone: aumenta dimensioni toggle per dark mode
    aumento le dimensioni del bottone toggle della dark mode

--- frontend-quiz-app/src/components/atoms/ToggleButton.tsx
+++ frontend-quiz-app/src/components/atoms/ToggleButton.tsx
@@ -8,23 +8,23 @@ export const ToggleButton: React.FC = () => {
     : iconDarkMode.dark;
 
   return (
-      <label className="inline-flex cursor-pointer items-center gap-2">
-        <span>
-          <IconSun aria-label="icon sun" className="size-5" />
-        </span>
-        <input
-          type="checkbox"
-          className="peer sr-only"
-          checked={darkMode}
-          onChange={toggleDarkMode}
-        />
-        <div className="peer relative h-6 w-10 rounded-full bg-purple after:absolute after:start-[4px]
-        after:top-[4px] after:h-4 after:w-4 after:rounded-full after:bg-pureWhite after:transition-all
-        after:content-[''] peer-checked:after:translate-x-full peer-focus:outline-none
-        rtl:peer-checked:after:-translate-x-full" />
-        <span>
-          <IconMoon aria-label="icon moon" className="size-5" />
-        </span>
-      </label>
+    <label className="inline-flex cursor-pointer items-center gap-2 md:gap-4">
+      <span>
+        <IconSun aria-label="icon sun" className="size-5 md:size-7" />
+      </span>
+      <input
+        type="checkbox"
+        className="peer sr-only"
+        checked={darkMode}
+        onChange={toggleDarkMode}
+      />
+      <div className="bg-purple after:bg-pureWhite peer relative h-6 w-10 rounded-full after:absolute
+      after:start-[4px] after:top-[4px] after:h-4 after:w-4 after:rounded-full after:transition-all
+      after:content-[''] peer-checked:after:translate-x-full peer-focus:outline-none md:h-8 md:w-14
+      md:after:h-6 md:after:w-6 rtl:peer-checked:after:-translate-x-full" />
+      <span>
+        <IconMoon aria-label="icon moon" className="size-5 md:size-7" />
+      </span>
+    </label>
   );
 };

398e161176797d22c591a8bb1a36362e217c96ba
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-24 14:28:37 +0200

    refactor: :truck: cambia importazioni delle immagini di background

--- frontend-quiz-app/src/assets/images/index.ts
+++ frontend-quiz-app/src/assets/images/index.ts
@@ -1,16 +1,32 @@
+// Icon Quiz
 import IconAccessibility from "./icon-accessibility.svg?react";
 import IconCss from "./icon-css.svg?react";
 import IconHtml from "./icon-html.svg?react";
 import IconJavascript from "./icon-js.svg?react";
+// Icon Dark mode
 import IconMoonDark from "./icon-moon-dark.svg?react";
 import IconMoonLight from "./icon-moon-light.svg?react";
 import IconSunDark from "./icon-sun-dark.svg?react";
 import IconSunLight from "./icon-sun-light.svg?react";
-import IconCorrect from './icon-correct.svg'
-import IconError from './icon-error.svg'
+import IconCorrect from "./icon-correct.svg";
+import IconError from "./icon-error.svg";
+// Background image
+import mobileLight from "./pattern-background-mobile-light.svg";
+import tabletLight from "./pattern-background-tablet-light.svg";
+import desktopLight from "./pattern-background-desktop-light.svg";
+import mobileDark from "./pattern-background-mobile-dark.svg";
+import tabletDark from "./pattern-background-tablet-dark.svg";
+import desktopDark from "./pattern-background-desktop-dark.svg";
 
 type IconComponents = {
   [key: string]: React.FC<React.SVGProps<SVGSVGElement>>;
+};
+
+export const iconMaps: IconComponents = {
+  IconCss,
+  IconHtml,
+  IconJavascript,
+  IconAccessibility,
 };
 
 type IconDarkMode = {
@@ -20,13 +36,6 @@ type IconDarkMode = {
   dark: {
     [key: string]: React.FC<React.SVGProps<SVGSVGElement>>;
   };
-};
-
-export const iconMaps: IconComponents = {
-  IconCss,
-  IconHtml,
-  IconJavascript,
-  IconAccessibility,
 };
 
 export const iconDarkMode: IconDarkMode = {
@@ -40,7 +49,16 @@ export const iconDarkMode: IconDarkMode = {
   },
 };
 
-export const iconAnswer ={
+export const iconAnswer = {
   IconCorrect,
-  IconError
-}
+  IconError,
+};
+
+export const backgroundImage = {
+  mobileLight,
+  tabletLight,
+  desktopLight,
+  mobileDark,
+  tabletDark,
+  desktopDark,
+};

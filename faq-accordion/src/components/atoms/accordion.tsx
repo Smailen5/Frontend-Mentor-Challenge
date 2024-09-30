13f7a84f230cf61985df97004e0006cf8e4f5a63
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-30 14:03:17 +0200

    style: :lipstick: cambia icona

--- faq-accordion/src/components/atoms/accordion.tsx
+++ faq-accordion/src/components/atoms/accordion.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import { ChevronDownIcon } from "@radix-ui/react-icons"
 
 import { cn } from "@/lib/utils"
+import { IconPlus } from "@/assets/images"
 
 const Accordion = AccordionPrimitive.Root
 
@@ -32,7 +32,7 @@ const AccordionTrigger = React.forwardRef<
       {...props}
     >
       {children}
-      <ChevronDownIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      <IconPlus/>
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))

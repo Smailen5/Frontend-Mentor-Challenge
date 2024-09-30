3e79f6c7bec4b35bb900eb03ba13c4b0fd798ab8
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-30 14:43:48 +0200

    refactor: :truck: esporta icone
    importo e esporto icone svg nel file di indice

--- faq-accordion/src/assets/images/index.ts
+++ faq-accordion/src/assets/images/index.ts
@@ -1,11 +1,7 @@
-import IconPlus from "./icon-plus.svg?react"
+import IconPlus from "./icon-plus.svg?react";
+import IconMinus from "./icon-minus.svg?react";
+import IconStar from "./icon-star.svg?react";
+import BackgroundMobile from "./background-pattern-mobile.svg?react";
+import backgroundDesktop from "./background-pattern-desktop.svg?react";
 
-type IconType = {
-    IconPlus: React.FC<React.SVGProps<SVGSVGElement>>;
-}
-
-export const icon:IconType = {
-    IconPlus
-}
-
-export { IconPlus }
+export { IconPlus, IconMinus, IconStar, BackgroundMobile, backgroundDesktop };

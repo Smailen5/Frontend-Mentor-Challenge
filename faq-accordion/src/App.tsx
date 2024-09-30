7949f3dee2bc4fdb4f4eda0816172126f507b6b7
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-30 13:30:54 +0200

    build: :construction_worker: installa svgr
    installo svgr per gestire le icone in svg

--- faq-accordion/src/App.tsx
+++ faq-accordion/src/App.tsx
@@ -1,5 +1,65 @@
+/// <reference types="vite-plugin-svgr/client" />
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@radix-ui/react-accordion";
+import { Background } from "./components/atoms/Background";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "./components/atoms/card";
+import { Page } from "./components/atoms/Page";
+// import { IconPlus } from "./assets/images/index";
+
 function App() {
-  return <>App</>;
+  return (
+    <>
+      {/* sezione page da esportare */}
+      <Background>
+        {/* sezione page che gestisce lo spazio */}
+        <Page>
+          {/* sezione card accordion */}
+          <Card>
+            <CardHeader>
+              <CardTitle>FAQs</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Accordion type="single" collapsible className="w-full">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>
+                    What is Frontend Mentor, and how will it help me?
+                    {/* <IconPlus /> */}
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                  <AccordionTrigger>Is it styled?</AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It comes with default styles that matches the other
+                    components&apos; aesthetic.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                  <AccordionTrigger>Is it animated?</AccordionTrigger>
+                  <AccordionContent>
+                    Yes. It's animated by default, but you can disable it if you
+                    prefer.
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </CardContent>
+          </Card>
+          {/* <section className="h-1/2 w-1/2 bg-card"></section> */}
+        </Page>
+      </Background>
+    </>
+  );
 }
 
 export default App;
---
+++ faq-accordion/src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import IconPlus from "./icon-plus.svg?react"
+
+type IconType = {
+    IconPlus: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+export const icon:IconType = {
+    IconPlus
+}
+
+export { IconPlus }

9fc9d4209c3895423563338856020a108adfb279
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-30 14:44:12 +0200

    style: :lipstick: aggiungi margine pagine
    aggiungo padding

--- faq-accordion/src/components/atoms/Page.tsx
+++ faq-accordion/src/components/atoms/Page.tsx
@@ -4,7 +4,7 @@ type PageProps = {
 
 export const Page: React.FC<PageProps> = ({ children }) => {
   return (
-    <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
+    <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center p-4">
       {children}
     </div>
   );

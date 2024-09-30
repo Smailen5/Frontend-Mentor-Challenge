01a289624b64720b678c3b1a1cb32439b4568620
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-09-30 15:13:45 +0200

    style: :art: struttura app
    aggiungo la struttura di app

--- faq-accordion/src/App.tsx
+++ faq-accordion/src/App.tsx
@@ -13,51 +13,74 @@ import {
   CardTitle,
 } from "./components/atoms/card";
 import { Page } from "./components/atoms/Page";
-// import { IconPlus } from "./assets/images/index";
+import { IconStar } from "./assets/images";
 
 function App() {
   return (
     <>
-      {/* sezione page da esportare */}
-      <Background>
-        {/* sezione page che gestisce lo spazio */}
-        <Page>
-          {/* sezione card accordion */}
-          <Card>
-            <CardHeader>
-              <CardTitle>FAQs</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Accordion type="single" collapsible className="w-full">
-                <AccordionItem value="item-1">
-                  <AccordionTrigger className="">
-                    What is Frontend Mentor, and how will it help me?
-                    {/* <IconPlus /> */}
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    Yes. It adheres to the WAI-ARIA design pattern.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                  <AccordionTrigger>Is it styled?</AccordionTrigger>
-                  <AccordionContent>
-                    Yes. It comes with default styles that matches the other
-                    components&apos; aesthetic.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                  <AccordionTrigger>Is it animated?</AccordionTrigger>
-                  <AccordionContent>
-                    Yes. It's animated by default, but you can disable it if you
-                    prefer.
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </CardContent>
-          </Card>
-          {/* <section className="h-1/2 w-1/2 bg-card"></section> */}
-        </Page>
-      </Background>
+      <div className="font-work-sans">
+        {/* sezione page da esportare */}
+        <Background>
+          {/* sezione page che gestisce lo spazio */}
+          <Page>
+            {/* sezione card accordion */}
+            <Card>
+              <CardHeader>
+                <IconStar className="size-6" />
+                <CardTitle>FAQs</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <Accordion type="single" collapsible className="w-full">
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className="">
+                      What is Frontend Mentor, and how will it help me?
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Similique repudiandae officiis ipsum perferendis,
+                      dignissimos ullam vel harum optio animi sint earum dicta
+                      explicabo eos velit porro esse, exercitationem reiciendis
+                      nisi.
+                    </AccordionContent>
+                  </AccordionItem>
+
+                  <AccordionItem value="item-2">
+                    <AccordionTrigger>
+                      Is Frontend Mentor free?
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    </AccordionContent>
+                  </AccordionItem>
+
+                  <AccordionItem value="item-3">
+                    <AccordionTrigger>
+                      Can i use Frontend Mentor projects in my portfolio?
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Similique repudiandae officiis ipsum perferendis,
+                      dignissimos ullam vel harum optio animi sint earum dicta
+                      explicabo eos velit porro esse, exercitationem reiciendis
+                      nisi.
+                    </AccordionContent>
+                  </AccordionItem>
+
+                  <AccordionItem value="item-4">
+                    <AccordionTrigger>
+                      How ca in get help if I'm stuck on a challenge?
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </CardContent>
+            </Card>
+          </Page>
+        </Background>
+      </div>
     </>
   );
 }

ef369fea8dc90e6af4a179d4b6ca17656f4a9cb6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-03 14:58:45 +0200

    refactor: :art: cambia struttura
    cambio la struttura di ContainerInput per gestire due tipi di container a seconda della prop

--- contact-form/src/components/atoms/containerInput.tsx
+++ contact-form/src/components/atoms/containerInput.tsx
@@ -1,7 +1,14 @@
 type ContainerInputProps = {
   children: React.ReactNode;
+  type?: string;
 };
 
-export const ContainerInput: React.FC<ContainerInputProps> = ({ children }) => {
-  return <div className="flex rounded-sm border-2 p-2">{children}</div>;
+export const ContainerInput: React.FC<ContainerInputProps> = ({
+  children,
+  type,
+}) => {
+  if (type === "radio") {
+    return <div className="flex rounded-sm border-2 p-2">{children}</div>;
+  }
+  return <div className="pb-4">{children}</div>;
 };

0d95480059e36300daa58b5c5c9471be9a08bc67
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-03 13:25:56 +0200

    feat: :sparkles: aggiungi formik
    aggiungo formik al form e setto il valore iniziale

--- contact-form/src/components/organisms/form.tsx
+++ contact-form/src/components/organisms/form.tsx
@@ -1,41 +1,62 @@
-// import { Form } from "formik";
-import { Page } from "../molecules/page";
-import { Label } from "../atoms/label";
-import { Input } from "../atoms/input";
+import { Form, Formik } from "formik";
 import { ContainerInput } from "../atoms/containerInput";
+import { Input } from "../atoms/input";
+import { Label } from "../atoms/label";
+import { Page } from "../molecules/page";
+import { Button } from "../atoms/button";
+
+const initialValues = {
+  name: "",
+  lastName: "",
+  email: "",
+  queryType: "",
+  message: "",
+  contact: "",
+};
 
 export const FormPage = () => {
   return (
     <Page>
-      <form>
-        <Label>First Name *</Label>
-        <Input />
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(value) => {
+          console.log(value);
+        }}
+      >
+        {() => (
+          <Form>
+            <Label>First Name *</Label>
+            <Input />
 
-        <Label>Last Name *</Label>
-        <Input />
+            <Label>Last Name *</Label>
+            <Input />
 
-        <Label>Email Address *</Label>
-        <Input />
+            <Label>Email Address *</Label>
+            <Input />
 
-        <fieldset>
-          <legend>Query Type *</legend>
-          <ContainerInput>
-            <input type="radio" />
-            <Label>General Enquiry</Label>
-          </ContainerInput>
+            <fieldset>
+              <legend>Query Type *</legend>
+              <ContainerInput>
+                <input type="radio" />
+                <Label>General Enquiry</Label>
+              </ContainerInput>
 
-          <ContainerInput>
-            <input type="radio" />
-            <Label>Support Request</Label>
-          </ContainerInput>
-        </fieldset>
+              <ContainerInput>
+                <input type="radio" />
+                <Label>Support Request</Label>
+              </ContainerInput>
+            </fieldset>
 
-        <Label>Message *</Label>
-        <Input type="text" />
+            <Label>Message *</Label>
+            <Input type="text" />
 
-        <input type="checkbox" />
-        <Label>I consent to being contacted by the team *</Label>
-      </form>
+            <input type="checkbox" />
+            <Label>I consent to being contacted by the team *</Label>
+
+            <Button type="submit">Submit</Button>
+          </Form>
+        )}
+      </Formik>
     </Page>
   );
 };

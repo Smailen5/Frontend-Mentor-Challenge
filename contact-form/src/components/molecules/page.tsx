f3784741f38953a108fa08f964f3e2513062709f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-03 12:47:36 +0200

    feat: :building_construction: crea componente
    creo componente page per gestire la posizone degli elementi in pagina

---
+++ contact-form/src/components/molecules/page.tsx
@@ -0,0 +1,21 @@
+import { Background } from "../atoms/background";
+import { Card, CardContent, CardHeader, CardTitle } from "../atoms/card";
+
+type PageProps = {
+  children?: React.ReactNode;
+};
+
+export const Page: React.FC<PageProps> = ({ children }) => {
+  return (
+    <Background>
+      <div className="flex items-center justify-center py-6">
+        <Card>
+          <CardHeader>
+            <CardTitle>Contact Us</CardTitle>
+            <CardContent>{children}</CardContent>
+          </CardHeader>
+        </Card>
+      </div>
+    </Background>
+  );
+};

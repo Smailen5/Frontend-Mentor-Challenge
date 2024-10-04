ed806a63b109ddd6d2c43cac85c618f8d27cd408
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-04 17:00:49 +0200

    refactor: :art: aggiungi prop
    aggiungo prop a circle per gestire l'attivazione

--- contact-form/src/components/atoms/circle.tsx
+++ contact-form/src/components/atoms/circle.tsx
@@ -1,7 +1,9 @@
-export const Circle = () => {
+export const Circle: React.FC<{ active?: boolean }> = ({ active }) => {
   return (
-    <div className="flex size-5 items-center justify-center rounded-full border border-input group-focus:border-primary group-active:border-primary">
-      <div className="size-3 rounded-full group-target:bg-primary group-focus:bg-primary" />
+    <div
+      className={`flex size-5 items-center justify-center rounded-full border border-input ${active && "border-2 border-primary"}`}
+    >
+      <div className={`size-3 rounded-full ${active && "bg-primary"}`} />
     </div>
   );
 };

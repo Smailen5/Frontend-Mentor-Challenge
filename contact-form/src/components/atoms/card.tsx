3b3acc3679bd6ab813fbf78b4c9dc5f91f22bd33
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-04 14:37:52 +0200

    style: :iphone: modifica dimensioni
    modifico dimensioni card per schermi piu grandi

--- contact-form/src/components/atoms/card.tsx
+++ contact-form/src/components/atoms/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "w-full max-w-screen-sm rounded-lg border bg-card text-card-foreground shadow-sm",
       className,
     )}
     {...props}
@@ -35,10 +35,7 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
-    className={cn(
-      "text-3xl font-bold leading-none tracking-tight",
-      className,
-    )}
+    className={cn("text-3xl font-bold leading-none tracking-tight", className)}
     {...props}
   />
 ));
@@ -52,9 +49,4 @@ const CardContent = React.forwardRef<
 ));
 CardContent.displayName = "CardContent";
 
-export {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-};
+export { Card, CardHeader, CardTitle, CardContent };

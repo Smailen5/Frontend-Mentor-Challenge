fe3ad195ac884ef763d37b31365028ba19c82c6d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-04 16:19:35 +0200

    style: :art: crea componente
    creo componente per visualizzare un radio button

---
+++ contact-form/src/components/atoms/circle.tsx
@@ -0,0 +1,7 @@
+export const Circle = () => {
+  return (
+    <div className="flex size-5 items-center justify-center rounded-full border border-input group-focus:border-primary group-active:border-primary">
+      <div className="size-3 rounded-full group-target:bg-primary group-focus:bg-primary" />
+    </div>
+  );
+};

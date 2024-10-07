fbf200595bc6f605882802fb87abdc43a480f9cf
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-07 12:08:04 +0200

    refactor: :art: crea indice
    creo indice per esportare le icone come svg

---
+++ contact-form/src/assets/images/index.tsx
@@ -0,0 +1,5 @@
+import Checkbox from "./icon-checkbox-check.svg?react";
+import Radio from "./icon-radio-selected.svg?react";
+import Success from "./icon-success-check.svg?react";
+
+export { Checkbox, Radio, Success };

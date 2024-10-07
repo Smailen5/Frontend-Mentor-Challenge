5fb55e868b684cfc52439f7cebc0879aca4c5ee2
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-07 12:43:12 +0200

    refactor: :truck: modifica importazione
    modifico importazione in stringa anziche svg

--- contact-form/src/assets/images/index.tsx
+++ contact-form/src/assets/images/index.tsx
@@ -1,4 +1,4 @@
-import Checkbox from "./icon-checkbox-check.svg?react";
+import Checkbox from "./icon-checkbox-check.svg";
 import Radio from "./icon-radio-selected.svg?react";
 import Success from "./icon-success-check.svg?react";
 

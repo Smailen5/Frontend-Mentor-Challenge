789bd426bcdee79b496f884ea92f44ff303da6f6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-01-16 15:00:24 +0100

    🎨 style: aggiunta variabili colore personalizzate
    - Implementate variabili colore custom in index.css per migliorare il tema
    - Aggiornato tailwind.config.js con nuove variabili colore
    - Ottimizzata configurazione per l'utilizzo dei colori personalizzati

--- ecommerce-product-page/tailwind.config.js
+++ ecommerce-product-page/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      orange: "var(--orange)",
+      "pale-orange": "var(--pale-orange)",
+      "very-dark-blue": "var(--very-dark-blue)",
+      "dark-grayish-blue": "var(--dark-grayish-blue)",
+      "grayish-blue": "var(--grayish-blue)",
+      "light-grayish-blue": "var(--light-grayish-blue)",
+      white: "var(--white)",
+      black: "var(--black)",
+    },
     extend: {},
   },
   plugins: [],
-}
+};

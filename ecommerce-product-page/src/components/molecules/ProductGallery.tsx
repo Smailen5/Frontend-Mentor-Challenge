b93756c58fc47df73862ebe195308eb003ec02d4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-02-18 14:01:40 +0100

    ✨ feat: implement interactive product image gallery
    - Added state management for current image selection
    - Enabled dynamic image switching through thumbnail clicks
    - Integrated image assets and created responsive gallery layout

--- ecommerce-product-page/src/components/molecules/ProductGallery.tsx
+++ ecommerce-product-page/src/components/molecules/ProductGallery.tsx
@@ -1,5 +1,38 @@
+import { imageProducts } from "@/assets/images";
+import { useState } from "react";
+import Image from "../atoms/Image";
+
 const ProductGallery = () => {
-  return <div>ProductGallery</div>;
+  const [currentImage, setCurrentImage] = useState<number>(0);
+
+  const handleImage = (index: number) => {
+    setCurrentImage(index);
+  };
+
+  console.log(currentImage);
+  return (
+    <div className="space-y-8">
+      <Image
+        src={imageProducts[currentImage]}
+        alt="image 1"
+        className="rounded-2xl"
+      />
+
+      <div className="flex h-20 w-full justify-between gap-4">
+        {imageProducts.map((image, index) => {
+          return (
+            <Image
+              key={index}
+              src={image}
+              alt="image 1"
+              className="rounded-xl"
+              onClick={() => handleImage(index)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ProductGallery;

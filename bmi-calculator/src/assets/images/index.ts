b6c517c86e12ecc15341baa1231fd8a3610a4890
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-03 18:16:45 +0100

    🔧 config: integrare supporto SVG e aggiornare configurazione Vite
    - Aggiungere plugin vite-plugin-svgr per gestione componenti SVG
    - Configurare vite.config.ts per supportare importazione SVG come componenti React
    - Aggiornare vite-env.d.ts con riferimento al plugin SVGR
    - Rimuovere font Inter e file correlati non più necessari

---
+++ bmi-calculator/src/assets/images/index.ts
@@ -0,0 +1,25 @@
+import IconAge from "./icon-age.svg?react";
+import IconEating from "./icon-eating.svg?react";
+import IconExercise from "./icon-exercise.svg?react";
+import IconGender from "./icon-gender.svg?react";
+import IconMuscle from "./icon-muscle.svg?react";
+import IconPregnancy from "./icon-pregnancy.svg?react";
+import IconRace from "./icon-race.svg?react";
+import IconSleep from "./icon-sleep.svg?react";
+import Logo from "./logo.svg?react";
+import PatternCurvedLeft from "./pattern-curved-line-left.svg?react";
+import PatternCurvedRight from "./pattern-curved-line-right.svg?react";
+
+export {
+  IconAge,
+  IconEating,
+  IconExercise,
+  IconGender,
+  IconMuscle,
+  IconPregnancy,
+  IconRace,
+  IconSleep,
+  Logo,
+  PatternCurvedLeft,
+  PatternCurvedRight,
+};

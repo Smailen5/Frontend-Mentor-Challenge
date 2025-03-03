77044d708aed698b5da926303e2422053486aba5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-03 18:23:49 +0100

    🖼️ assets: aggiungere immagine ManEating e organizzare import
    - Importare immagine ManEating in formato webp
    - Aggiungere commenti per categorizzare gli import (Icons, Logo, Patterns, Images)
    - Esportare nuova immagine ManEating dall'indice degli asset

--- bmi-calculator/src/assets/images/index.ts
+++ bmi-calculator/src/assets/images/index.ts
@@ -1,3 +1,4 @@
+// Icons
 import IconAge from "./icon-age.svg?react";
 import IconEating from "./icon-eating.svg?react";
 import IconExercise from "./icon-exercise.svg?react";
@@ -6,9 +7,13 @@ import IconMuscle from "./icon-muscle.svg?react";
 import IconPregnancy from "./icon-pregnancy.svg?react";
 import IconRace from "./icon-race.svg?react";
 import IconSleep from "./icon-sleep.svg?react";
+// Logo
 import Logo from "./logo.svg?react";
+// Patterns
 import PatternCurvedLeft from "./pattern-curved-line-left.svg?react";
 import PatternCurvedRight from "./pattern-curved-line-right.svg?react";
+// Images
+import ManEating from "./image-man-eating.webp";
 
 export {
   IconAge,
@@ -22,4 +27,5 @@ export {
   Logo,
   PatternCurvedLeft,
   PatternCurvedRight,
+  ManEating,
 };

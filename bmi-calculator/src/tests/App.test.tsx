9748e6628390bcf3ba573db5b8e2f18eccd9dede
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-04 13:24:47 +0100

    🧪 test: aggiungere test per il rendering del componente App
    - Verificare che il componente App venga renderizzato correttamente
    - Controllare la presenza del testo "Esempio di Preset 1" nel documento
    - Utilizzare Vitest e Testing Library per il test

--- bmi-calculator/src/tests/App.test.tsx
+++ bmi-calculator/src/tests/App.test.tsx
@@ -2,9 +2,12 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import App from "../App";
 
+// Test per verificare che il componente App venga renderizzato correttamente
 describe("App", () => {
   it("should render", () => {
+    // renderizza il componente App
     render(<App />);
+    // verifica che il testo "Esempio di Preset 1" sia presente nel documento
     expect(screen.getByText("Esempio di Preset 1")).toBeInTheDocument();
   });
 });

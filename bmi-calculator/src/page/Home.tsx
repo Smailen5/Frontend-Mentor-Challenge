741ed4668c179643602bf4b5aaed4f2abbe9142f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-05 12:50:34 +0100

    ✨ feat: aggiungere sezione di contenuto principale nella Home
    - Importare il contenuto principale da un file JSON
    - Aggiungere un titolo e una descrizione sotto il CardForm
    - Migliorare la pagina con informazioni testuali aggiuntive

--- bmi-calculator/src/page/Home.tsx
+++ bmi-calculator/src/page/Home.tsx
@@ -1,6 +1,7 @@
 import { ManEating } from "@/assets/images";
 import IntroHeader from "@/components/molecules/IntroHeader";
 import CardForm from "@/components/organisms/CardForm";
+import mainContent from "@/data/mainContent.json";
 
 const Home = () => {
   return (
@@ -10,6 +11,13 @@ const Home = () => {
       <CardForm />
 
       <img src={ManEating} alt="Man Eating" className="scale-110" />
+
+      <div className="mt-16 px-2 space-y-8">
+        <h4 className="text-preset-3 text-blue-900">{mainContent[0].title}</h4>
+        <p className="text-preset-6-regular text-grey-500">
+          {mainContent[0].description}
+        </p>
+      </div>
     </>
   );
 };

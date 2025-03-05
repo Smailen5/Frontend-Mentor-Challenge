cdf033c1d81a92ab6f10de59191cc6a6b279d320
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-05 09:55:27 +0100

    ✨ feat: migliorare layout e stile dei componenti
    - Aggiungere sfondo con gradiente nel componente Layout
    - Modificare lo stile degli input radio in InputMisure
    - Implementare classi personalizzate per stato checked degli elementi

--- bmi-calculator/src/components/atoms/Layout.tsx
+++ bmi-calculator/src/components/atoms/Layout.tsx
@@ -4,7 +4,9 @@ import { LayoutProps } from "@/types";
 const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-      <div className="container mx-auto px-4 pt-8">
+      <div className="relative container mx-auto px-4 pt-8">
+        {/* Background gradiente */}
+        <div className="from-gradient-2 absolute inset-0 -z-10 h-3/4 w-full rounded-br-4xl bg-linear-to-l to-white" />
         <div className="flex w-full justify-center">
           <Logo className="size-10" />
         </div>
--- bmi-calculator/src/components/molecules/InputMisure.tsx
+++ bmi-calculator/src/components/molecules/InputMisure.tsx
@@ -5,13 +5,15 @@ interface InputMisureProps {
 const InputMisure = ({ name }: InputMisureProps) => {
   return (
     <div className="flex items-center space-x-4">
-      <input
-        type="radio"
-        name="misure"
-        id={name}
-        className="size-8 appearance-none rounded-full bg-blue-300"
-      />
-      <label htmlFor={name} className="text-white">
+      <div className='rounded-full has-checked:bg-blue-300 flex items-center justify-center size-8 has-not-checked:border has-not-checked:border-white'>
+        <input
+          type="radio"
+          name="misure"
+          id={name}
+          className="size-4 appearance-none rounded-full checked:bg-blue-500"
+        />
+      </div>
+      <label htmlFor={name} className="text-white has-checked:bg-blue-300">
         {name}
       </label>
     </div>

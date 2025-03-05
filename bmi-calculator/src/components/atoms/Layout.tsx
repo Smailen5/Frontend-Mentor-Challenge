9fecb50e46b1540b8db2129ee5c032a20c6a24ac
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-05 12:24:18 +0100

    🎨 style: adattare l'altezza del gradiente di sfondo nel Layout
    - Modificare la classe del div del gradiente per limitare l'altezza massima
    - Utilizzare calc() per definire un'altezza proporzionale alla pagina
    - Mantenere il design del gradiente con un'altezza più contenuta

--- bmi-calculator/src/components/atoms/Layout.tsx
+++ bmi-calculator/src/components/atoms/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: LayoutProps) => {
     <>
       <div className="relative container mx-auto px-4 pt-8">
         {/* Background gradiente */}
-        <div className="from-gradient-2 absolute inset-0 -z-10 h-3/4 w-full rounded-br-4xl bg-linear-to-l to-white" />
+        <div className="from-gradient-2 absolute inset-0 -z-10 max-h-[calc(50%-5rem)] w-full rounded-br-4xl bg-linear-to-l to-white" />
         <div className="flex w-full justify-center">
           <Logo className="size-10" />
         </div>

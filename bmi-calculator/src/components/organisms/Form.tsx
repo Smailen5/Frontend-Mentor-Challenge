f6322dc13b72c6bb4dbbc6d6b34cf2993a78c892
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-05 12:04:29 +0100

    ✨ feat: aggiungere sezione di risultato BMI nel Form
    - Migliorare il layout del componente Form con nuova sezione di risultato
    - Aggiungere un messaggio di benvenuto e istruzioni per il calcolo del BMI
    - Modificare le classi di stile per adattarsi al nuovo design

--- bmi-calculator/src/components/organisms/Form.tsx
+++ bmi-calculator/src/components/organisms/Form.tsx
@@ -3,21 +3,29 @@ import InputMisure from "../molecules/MeasurementSystemRadio";
 
 const Form = () => {
   return (
-    <div className="mx-2 mt-8 flex h-96 w-auto flex-col space-y-6 rounded-xl bg-white px-6 shadow-2xl shadow-blue-100">
-      <h3 className="text-preset-4 pt-6">Enter your details below</h3>
+    <>
+      <div className="mx-2 mt-8 flex h-auto w-auto flex-col space-y-6 rounded-xl bg-white p-6 shadow-2xl shadow-blue-100">
+        <h3 className="text-preset-4">Enter your details below</h3>
 
-      <div className="flex items-center justify-between">
-        <InputMisure name="metric" />
+        <div className="flex items-center justify-between">
+          <InputMisure name="metric" />
 
-        <InputMisure name="imperial" />
+          <InputMisure name="imperial" />
+        </div>
+
+        <div className="space-y-4">
+          <MeasureInput name="height" unit="cm" />
+
+          <MeasureInput name="weight" unit="kg" />
+        </div>
+        <div className="h-auto w-full space-y-5 rounded-xl bg-blue-500 p-8 text-white">
+          <h4 className="text-preset-4 capitalize">welcome!</h4>
+          <p className="text-preset-7-regular">
+            Enter your height and weight and you'll see your BMI result here
+          </p>
+        </div>
       </div>
-
-      <div className="space-y-4">
-        <MeasureInput name="height" unit="cm" />
-
-        <MeasureInput name="weight" unit="kg" />
-      </div>
-    </div>
+    </>
   );
 };
 

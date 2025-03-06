f3e6988648cdddb551da6de0b1c8a905fe415555
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-06 13:57:08 +0100

    ✨ feat: migliorare il componente Card con mapping dinamico delle icone
    - Aggiornare il componente Card per utilizzare un mapping dinamico delle icone
    - Modificare il file benefitCards.json per supportare il nuovo sistema di icone
    - Importare le icone da un file di asset esterno

--- bmi-calculator/src/components/molecules/Card.tsx
+++ bmi-calculator/src/components/molecules/Card.tsx
@@ -1,9 +1,17 @@
+import { IconEating, IconExercise, IconSleep } from "@/assets/images";
 import { CardProps } from "@/types";
 
+const iconMap = {
+  IconEating: IconEating,
+  IconExercise: IconExercise,
+  IconSleep: IconSleep,
+};
+
 const Card = ({ icon, title, description }: CardProps) => {
+  const IconComponent = iconMap[icon as keyof typeof iconMap];
   return (
     <>
-      {icon}
+      <IconComponent />
       <div className="space-y-6">
         <h4 className="text-preset-4 text-blue-900">{title}</h4>
         <p className="text-preset-6-regular text-grey-500">{description}</p>

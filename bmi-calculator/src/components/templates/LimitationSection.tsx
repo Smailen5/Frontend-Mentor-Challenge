0401bef7e68660fd4342020e905ad162feae085a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-10 13:04:50 +0100

    🎨 style: migliorare l'aspetto visivo delle card di limitazione
    - Aggiungere ombreggiatura e sfondo bianco al LimitationCard
    - Modificare la spaziatura verticale nella LimitationSection
    - Rimuovere il margine inferiore nella sezione delle limitazioni

--- bmi-calculator/src/components/templates/LimitationSection.tsx
+++ bmi-calculator/src/components/templates/LimitationSection.tsx
@@ -7,8 +7,8 @@ const LimitationSection = () => {
   return (
     <>
       <Layout>
-        <div className="mx-2 space-y-4">
-          <div className="space-y-8 py-10 text-center">
+        <div className="mx-2 space-y-14">
+          <div className="space-y-8 pt-10 text-center">
             <h2 className="text-preset-3 text-blue-900">{limitation.title}</h2>
             <p className="text-grey-500 text-preset-6-regular">
               {limitation.description}

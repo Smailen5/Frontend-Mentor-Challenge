73ba8989618e6e566edc867575739ed0160b0c8c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-10 14:01:59 +0100

    🎨 style: migliorare lo sfondo gradiente in CardsSection
    - Aggiungere un div con gradiente assoluto per estendere l'effetto visivo
    - Commentare la necessità di rivedere il componente Layout per un migliore padding
    - Mantenere la struttura esistente della sezione delle card

--- bmi-calculator/src/components/templates/CardsSection.tsx
+++ bmi-calculator/src/components/templates/CardsSection.tsx
@@ -5,6 +5,9 @@ const CardsSection = () => {
   return (
     <>
       <Layout>
+        {/* la sfumatura del gradiente deve scendere ancora sotto il componente, aggiungendo altro padding si sminchia il layout di tutta la pagina, va rivisto Layout */}
+        <div className="from-gradient-2/25 absolute inset-0 -z-10 bg-gradient-to-l to-white" />
+        {/* aggiungere pb-16 */}
         <div className="mx-2 space-y-10 pt-6">
           <BenefitCards />
         </div>

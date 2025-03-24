09b041280511a331388167f2df32edf6e30d098d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-03-24 13:28:47 +0100

    🎨 refactor: Migliorare la visualizzazione del titolo nell'IntroHeader
    - Aggiornare il componente IntroHeader per suddividere il titolo in due righe
    - Modificare lo stile del titolo per migliorare la leggibilità su schermi di diverse dimensioni

--- bmi-calculator/src/components/molecules/IntroHeader.tsx
+++ bmi-calculator/src/components/molecules/IntroHeader.tsx
@@ -3,7 +3,11 @@ import header from "@/data/headerText.json";
 const IntroHeader = () => {
   return (
     <div className="mx-auto space-y-6 pt-8 text-center">
-      <h1 className="text-preset-2 font-semibold capitalize">{header.title}</h1>
+      <h1 className="text-preset-2 font-semibold text-blue-900 capitalize md:mt-2 md:px-44 md:text-center">
+        {header.title.split(" ").slice(0, 2).join(" ")}
+        <br className="hidden md:block" />
+        {header.title.split(" ").slice(2).join(" ")}
+      </h1>
       <p className="px-3">{header.description}</p>
     </div>
   );

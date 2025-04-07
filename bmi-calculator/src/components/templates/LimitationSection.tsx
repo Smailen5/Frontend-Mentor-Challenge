d8c4cbafb6b17a294ef97547538bb2aee79b0fc1
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-04-07 14:00:22 +0200

    🎨 refactor: Migliorare la responsività e l'aspetto visivo dei componenti
    - Aggiornare il componente Card per una migliore gestione del layout su schermi di diverse dimensioni
    - Modificare il componente LimitationSection per migliorare la presentazione e la flessibilità dei contenuti

--- bmi-calculator/src/components/templates/LimitationSection.tsx
+++ bmi-calculator/src/components/templates/LimitationSection.tsx
@@ -8,14 +8,14 @@ const LimitationSection = () => {
     <>
       <Layout>
         <div className="mx-2 space-y-14">
-          <div className="space-y-8 pt-10 text-center">
+          <div className="space-y-8 pt-10 text-center md:pt-4">
             <h2 className="text-preset-3 text-blue-900">{limitation.title}</h2>
             <p className="text-grey-500 text-preset-6-regular">
               {limitation.description}
             </p>
           </div>
 
-          <div className="space-y-4">
+          <div className="space-y-4 md:flex md:flex-wrap md:space-y-0 md:gap-x-4 md:gap-y-6 md:justify-center">
             {cards.map((card, index) => {
               const { icon, title, description } = card;
               return (

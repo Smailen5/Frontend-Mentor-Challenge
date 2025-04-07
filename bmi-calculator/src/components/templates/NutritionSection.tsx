3960919fd28601b639fb2ea05adb673ffb7c643d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-04-07 15:08:36 +0200

    🎨 refactor: Aggiornare la responsività e l'aspetto visivo della NutritionSection
    - Aggiungere classi per migliorare la presentazione su schermi extra-large, inclusi aggiornamenti per dimensioni e padding.
    - Modificare il file index.css per includere nuove regole di stile responsive per schermi xl.

--- bmi-calculator/src/components/templates/NutritionSection.tsx
+++ bmi-calculator/src/components/templates/NutritionSection.tsx
@@ -3,16 +3,16 @@ import mainContent from "@/data/mainContent.json";
 import Layout from "../atoms/Layout";
 const NutritionSection = () => {
   return (
-    <div className="space-y-4 md:relative md:top-6 md:-left-20 md:flex md:w-[840px] md:gap-4">
+    <div className="space-y-4 md:relative md:top-6 md:-left-20 md:flex md:w-[840px] md:gap-4 xl:top-0 xl:left-0 xl:w-full xl:px-36 xl:gap-28">
       <img
         src={ManEating}
         alt="Man Eating"
-        className="md:relative md:h-[411px] md:w-[435px]"
+        className="md:relative md:h-[411px] md:w-[435px] xl:h-[533px] xl:w-[564px]"
       />
 
-      <Layout>
+      <Layout className="xl:p-0 xl:flex xl:items-end xl:pb-20">
         <div className="space-y-8 px-2">
-          <h4 className="text-preset-3 text-blue-900">
+          <h4 className="text-preset-3 xl:text-preset-2 text-blue-900">
             {mainContent[0].title}
           </h4>
           <p className="text-preset-6-regular text-grey-500">

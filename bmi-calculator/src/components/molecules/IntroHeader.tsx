0223ea2c65ee40895ac98d0ffaddd0fd2d7f4d5f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-04-07 14:38:55 +0200

    🎨 refactor: Ottimizzare la responsività e l'aspetto visivo dell'IntroHeader
    - Aggiornare il layout dell'IntroHeader per migliorare la presentazione su schermi extra-large
    - Modificare le classi di stile per una migliore leggibilità e allineamento del titolo e della descrizione

--- bmi-calculator/src/components/molecules/IntroHeader.tsx
+++ bmi-calculator/src/components/molecules/IntroHeader.tsx
@@ -2,13 +2,13 @@ import header from "@/data/headerText.json";
 
 const IntroHeader = () => {
   return (
-    <div className="mx-auto space-y-6 pt-8 text-center xl:w-[calc(50%-16px)]">
-      <h1 className="text-preset-2 font-semibold text-blue-900 capitalize md:mt-2 md:px-44 md:text-center xl:text-preset-1">
+    <div className="mx-auto space-y-6 pt-8 text-center xl:w-[calc(50%-16px)] xl:space-y-8 xl:text-left">
+      <h1 className="text-preset-2 xl:text-preset-1 font-semibold text-blue-900 capitalize md:mt-2 md:px-44 md:text-center xl:px-0 xl:pt-8 xl:text-left">
         {header.title.split(" ").slice(0, 2).join(" ")}
         <br className="hidden md:block" />{" "}
         {header.title.split(" ").slice(2).join(" ")}
       </h1>
-      <p className="text-preset-6-regular text-grey-500 px-3">
+      <p className="text-preset-6-regular text-grey-500 px-3 xl:w-11/12 xl:px-0">
         {header.description}
       </p>
     </div>

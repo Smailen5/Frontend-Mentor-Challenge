86b138098aac9154830383557622c253b86eccf0
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-04-07 14:19:47 +0200

    🎨 refactor: Migliorare la responsività e l'aspetto visivo del Layout e della HeroSection
    - Aggiornare il componente Layout per includere classi personalizzate e migliorare la responsività
    - Modificare la HeroSection per ottimizzare la disposizione degli elementi su schermi di diverse dimensioni

--- bmi-calculator/src/components/atoms/Layout.tsx
+++ bmi-calculator/src/components/atoms/Layout.tsx
@@ -4,7 +4,14 @@ import { twMerge } from "tailwind-merge";
 const Layout = ({ children, className }: LayoutProps) => {
   return (
     <>
-      <div className={twMerge("relative container mx-auto px-4 pt-8", className)}>{children}</div>
+      <div
+        className={twMerge(
+          "relative container mx-auto px-4 pt-8 xl:px-16 xl:py-[88px]",
+          className,
+        )}
+      >
+        {children}
+      </div>
     </>
   );
 };

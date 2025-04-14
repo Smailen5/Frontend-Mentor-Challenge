ddf454bd27edcb91706548db4e6c382d65094805
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-04-14 14:08:01 +0200

    🎨 refactor: Aggiornare il componente LimitationCard e migliorare la gestione delle righe nella LimitationSection
    - Aggiunta di classi per gestire meglio le dimensioni massime del componente LimitationCard su schermi extra-large.
    - Modifica della funzione chunkCards per restituire anche il numero di riga, migliorando l'allineamento delle cards nel layout.

--- bmi-calculator/src/components/templates/LimitationSection.tsx
+++ bmi-calculator/src/components/templates/LimitationSection.tsx
@@ -11,19 +11,24 @@ interface Card {
 
 const LimitationSection = () => {
   // funzione per dividere le cards in righe (spostala in utils)
-  function chunkCards(cards: Card[]): Card[][] {
-    const rows: Card[][] = [];
+  function chunkCards(cards: Card[]): { cards: Card[]; rowNumber: number }[] {
+    const rows: { cards: Card[]; rowNumber: number }[] = [];
     let index = 0;
+    let rowNumber = 1;
     for (let rowSize = 1; index < cards.length; rowSize++) {
-      rows.push(cards.slice(index, index + rowSize));
+      rows.push({
+        cards: cards.slice(index, index + rowSize),
+        rowNumber: rowNumber,
+      });
       index += rowSize;
+      rowNumber++;
     }
     return rows;
   }
 
   return (
     <Layout className="xl:py-[104px]">
-      <div className="mx-2 space-y-14 xl:m-0 xl:relative">
+      <div className="mx-2 space-y-14 xl:relative xl:m-0 xl:min-h-[704px]">
         <div className="space-y-8 pt-10 text-center md:pt-4 xl:max-w-[564px] xl:p-0 xl:text-left">
           <h2 className="text-preset-3 xl:text-preset-2 text-blue-900">
             {limitation.title}
@@ -46,16 +51,17 @@ const LimitationSection = () => {
         </div>
 
         {/* Desktop Layout */}
-        <div className="hidden xl:flex xl:flex-col xl:space-y-4 xl:absolute top-0">
-          {chunkCards(cards).map((row, rowIndex) => (
+        <div className="top-0 right-0 hidden xl:absolute xl:flex xl:max-w-[961px] xl:flex-col xl:space-y-8">
+          {chunkCards(cards).map((row, index) => (
             <div
-              key={rowIndex}
-              className="flex items-start gap-4"
+              key={index}
+              className="flex justify-end gap-8 flex-1 xl:max-w-[961px]"
               style={{
-                marginRight: `${rowIndex * 2}rem`,
+                marginRight: `${row.rowNumber % 2 === 0 ? 0 : row.rowNumber * 4}rem`,
+                marginLeft: `${row.rowNumber % 2 === 0 ? row.rowNumber * 4 : 0}rem`,
               }}
             >
-              {row.map((card, index) => (
+              {row.cards.map((card, index) => (
                 <LimitationCard
                   key={index}
                   title={card.title}

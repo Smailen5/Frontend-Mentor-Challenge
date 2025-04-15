281c212eff2d417d66a0a9e1518f4826f126762a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-04-15 14:48:32 +0200

    🎨 refactor: Aggiornare il layout della NutritionSection per migliorare la responsività
    - Modificato il layout della NutritionSection per ottimizzare la presentazione su schermi di diverse dimensioni, inclusi aggiornamenti alle classi di stile.
    - Aggiunto padding per migliorare l'allineamento e la spaziatura degli elementi nella sezione.

--- bmi-calculator/src/components/templates/NutritionSection.tsx
+++ bmi-calculator/src/components/templates/NutritionSection.tsx
@@ -3,14 +3,14 @@ import mainContent from "@/data/mainContent.json";
 import Layout from "../atoms/Layout";
 const NutritionSection = () => {
   return (
-    <div className="space-y-4 md:relative md:top-6 md:-left-20 md:flex md:w-[840px] md:gap-4 xl:top-0 xl:left-0 xl:w-full xl:px-36 xl:gap-28">
+    <div className="space-y-4 md:relative md:top-6 md:-left-20 md:flex md:w-[840px] md:gap-4 lg:w-full xl:top-0 xl:left-0 xl:w-full xl:gap-28 xl:px-36">
       <img
         src={ManEating}
         alt="Man Eating"
         className="md:relative md:h-[411px] md:w-[435px] xl:h-[533px] xl:w-[564px]"
       />
 
-      <Layout className="xl:p-0 xl:flex xl:items-end xl:pb-20">
+      <Layout className="xl:flex xl:items-end xl:p-0 xl:pb-20">
         <div className="space-y-8 px-2">
           <h4 className="text-preset-3 xl:text-preset-2 text-blue-900">
             {mainContent[0].title}

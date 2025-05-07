eed57b612bba62e95d3c90b5bbd9b60293ce6efb
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-07 18:38:26 +0200

    ♻️ refactor: semplificare il componente SwitchSelection rimuovendo codice commentato
    - Rimosso il codice commentato e semplificata la gestione della selezione del giocatore nel componente SwitchSelection, migliorando la leggibilità e l'integrazione con lo stato globale.

--- tic-tac-toe/src/components/molecules/SwitchSelection.tsx
+++ tic-tac-toe/src/components/molecules/SwitchSelection.tsx
@@ -1,25 +1,13 @@
-// import { useState } from "react";
 import { IconO, IconX } from "../../assets";
 import { useGameStore } from "../../store/gameStore";
 import { PlayerMarkButton } from "../atoms";
 
-// interface SwitchSelectionProps {
-//   initialSelection?: "player-x" | "player-o";
-// }
-
 const SwitchSelection = () => {
-  // const [playerSelection, setPlayerSelection] = useState<
-  //   "player-x" | "player-o"
-  // >(initialSelection);
   const { selectedPlayer, setSelectedPlayer } = useGameStore();
-  // setSelectedPlayer(initialSelection);
 
   const handlePlayerSelection = (selection: "player-x" | "player-o") => {
-    // setPlayerSelection(selection);
     setSelectedPlayer(selection);
   };
-
-  console.log(selectedPlayer)
 
   return (
     <div className="bg-dark-navy rounded-2xl p-2">

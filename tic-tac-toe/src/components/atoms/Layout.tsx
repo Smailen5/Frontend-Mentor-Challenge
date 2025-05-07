7233d159a2af84acb2fc89d4414f210a6686cbca
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-07 17:38:06 +0200

    ✨ feat: aggiornare il componente Layout per supportare classi personalizzate
    - Aggiunta la prop className al componente Layout, consentendo l'uso di stili personalizzati. Utilizzato twMerge per unire le classi predefinite con quelle fornite, migliorando la flessibilità del layout.

--- tic-tac-toe/src/components/atoms/Layout.tsx
+++ tic-tac-toe/src/components/atoms/Layout.tsx
@@ -1,10 +1,13 @@
+import { twMerge } from 'tailwind-merge';
+
 interface LayoutProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, className }: LayoutProps) => {
   return (
-    <main className="mx-auto flex h-screen w-full flex-col items-center justify-center px-4 py-4">
+    <main className={twMerge("mx-auto flex h-screen w-full flex-col items-center justify-center px-4 py-4", className)}>
       {children}
     </main>
   );

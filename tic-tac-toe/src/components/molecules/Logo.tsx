9e1d7557f82ae8dfa9e1b4c8b8b6121899517fc2
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-07 17:00:05 +0200

    🎨 style: aggiornare i colori delle icone nel componente Logo per migliorare l'estetica visiva
    - Modificati i colori delle icone X e O nel componente Logo, aggiungendo classi di colore personalizzate per una migliore distinzione visiva e coerenza con il tema del gioco.

--- tic-tac-toe/src/components/molecules/Logo.tsx
+++ tic-tac-toe/src/components/molecules/Logo.tsx
@@ -3,8 +3,8 @@ import { IconO, IconX } from "../../assets";
 const Logo = () => {
   return (
     <div className="flex items-center gap-2">
-      <IconX className="size-8" />
-      <IconO className="size-8" />
+      <IconX className="size-8 text-light-blue" />
+      <IconO className="size-8 text-light-yellow" />
     </div>
   );
 };

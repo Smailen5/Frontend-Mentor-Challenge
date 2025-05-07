4c32a4b66b38b7882bc6aedae73d9ef054c09eb4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-07 16:47:10 +0200

    ✨ feat: aggiornare il componente Card per supportare classi personalizzate
    - Modificato il componente Card per accettare una prop className, consentendo l'uso di stili personalizzati. Utilizzato twMerge per unire le classi predefinite con quelle fornite, migliorando la flessibilità del componente.

--- tic-tac-toe/src/components/atoms/Card.tsx
+++ tic-tac-toe/src/components/atoms/Card.tsx
@@ -1,10 +1,13 @@
+import { twMerge } from 'tailwind-merge';
+
 interface CardProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Card = ({ children }: CardProps) => {
+const Card = ({ children, className }: CardProps) => {
   return (
-    <div className="bg-semi-dark-navy text-silver rounded-2xl p-4 uppercase shadow-[0_8px_0_0_rgba(0,0,0,0.25)]">
+    <div className={twMerge("bg-semi-dark-navy text-silver rounded-2xl p-4 uppercase shadow-[0_8px_0_0_rgba(0,0,0,0.25)]",className)}>
       {children}
     </div>
   );

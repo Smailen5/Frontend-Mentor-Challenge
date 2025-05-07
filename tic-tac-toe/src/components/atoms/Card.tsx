f264b59616c78c2ce917969c74d50f621e946583
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-07 16:40:38 +0200

    🎨 style: aggiungere un'ombra al componente Card per migliorare l'aspetto visivo
    - Aggiornato il componente Card per includere un'ombra, migliorando l'interazione visiva e l'estetica generale del componente.

--- tic-tac-toe/src/components/atoms/Card.tsx
+++ tic-tac-toe/src/components/atoms/Card.tsx
@@ -4,7 +4,7 @@ interface CardProps {
 
 const Card = ({ children }: CardProps) => {
   return (
-    <div className="bg-semi-dark-navy text-silver rounded-2xl p-4 uppercase">
+    <div className="bg-semi-dark-navy text-silver rounded-2xl p-4 uppercase shadow-[0_8px_0_0_rgba(0,0,0,0.25)]">
       {children}
     </div>
   );

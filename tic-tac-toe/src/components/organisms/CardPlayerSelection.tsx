55dac32593d1fde4447ff5aca63c6725891a59b5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-07 16:50:43 +0200

    ✨ feat: aggiornare il componente CardPlayerSelection per includere la selezione del giocatore
    - Modificato il componente CardPlayerSelection per visualizzare un messaggio di selezione del giocatore e includere il componente SwitchSelection. Aggiunto un promemoria visivo per la sequenza di gioco, migliorando l'esperienza utente.

--- tic-tac-toe/src/components/organisms/CardPlayerSelection.tsx
+++ tic-tac-toe/src/components/organisms/CardPlayerSelection.tsx
@@ -1,5 +1,16 @@
+import { Card } from "../atoms";
+import SwitchSelection from "../molecules/SwitchSelection";
+
 const CardSelection = () => {
-  return <div>CardSelection</div>;
+  return (
+    <Card className="text-center space-y-6">
+      <h4 className="heading-xs">pick player 1's mark</h4>
+
+      <SwitchSelection />
+
+      <p className="body-m">remember : x goes first</p>
+    </Card>
+  );
 };
 
 export default CardSelection;

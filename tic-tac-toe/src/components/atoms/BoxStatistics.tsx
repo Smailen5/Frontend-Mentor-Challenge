57c788a2d3a471d676e661d1f8884f4d23b54e94
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 15:32:07 +0200

    ✨ feat: aggiornare il componente BoxStatistics per supportare i figli e le classi personalizzate
    - Modificato il componente BoxStatistics per accettare proprietà children e className, utilizzando twMerge per gestire le classi Tailwind, migliorando la flessibilità e la personalizzazione del componente.

--- tic-tac-toe/src/components/atoms/BoxStatistics.tsx
+++ tic-tac-toe/src/components/atoms/BoxStatistics.tsx
@@ -1,5 +1,21 @@
-const BoxStatistics = () => {
-  return <div className="bg-light-blue size-20 rounded-lg"></div>;
+import { twMerge } from "tailwind-merge";
+
+interface BoxStatisticsProps {
+  children: React.ReactNode;
+  className: string;
+}
+
+const BoxStatistics = ({ children, className }: BoxStatisticsProps) => {
+  return (
+    <div
+      className={twMerge(
+        "flex h-16 w-24 flex-col justify-center rounded-lg text-center uppercase",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default BoxStatistics;

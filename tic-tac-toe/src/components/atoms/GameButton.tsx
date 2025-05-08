9a37280a59220b36ab12305bfd9192cd1a70760c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 16:58:40 +0200

    ✨ feat: migliorare la visualizzazione del pulsante GameButton
    - Aggiornato il componente GameButton per includere classi CSS che migliorano l'allineamento e la presentazione delle icone per i giocatori X e O, rendendo l'interfaccia utente più coerente e visivamente accattivante.

--- tic-tac-toe/src/components/atoms/GameButton.tsx
+++ tic-tac-toe/src/components/atoms/GameButton.tsx
@@ -11,9 +11,14 @@ const GameButton = ({ position, value }: GameButtonProps) => {
   return (
     <button
       onClick={handleClick}
-      className="bg-semi-dark-navy size-20 cursor-pointer rounded-lg shadow-[0_8px_0_0_rgba(0,0,0,0.25)]"
+      className="bg-semi-dark-navy flex size-20 cursor-pointer items-center justify-center rounded-lg shadow-[0_8px_0_0_rgba(0,0,0,0.25)]"
     >
-      {value &&(value === "player-x" ? <IconX /> : <IconO />)}
+      {value &&
+        (value === "player-x" ? (
+          <IconX className="text-light-blue size-8" />
+        ) : (
+          <IconO className="text-light-yellow size-8" />
+        ))}
     </button>
   );
 };

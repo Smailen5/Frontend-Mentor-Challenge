e2e61298f4ab752c2548e6b0470645c8cfb4e2ad
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 18:56:19 +0200

    ♻️ refactor: aggiornare il componente WinnerMessage per migliorare la tipografia
    - Modificato il componente WinnerMessage per utilizzare un elemento <p> al posto di un <div> e aggiornato il className per migliorare la tipografia. Questa modifica rende il messaggio del vincitore più coerente con il design dell'applicazione Tic Tac Toe.

--- tic-tac-toe/src/components/molecules/WinnerMessage.tsx
+++ tic-tac-toe/src/components/molecules/WinnerMessage.tsx
@@ -4,15 +4,15 @@ import { WinnerMessageProps } from "../../types/molecules.types";
 const WinnerMessage = ({ winner }: WinnerMessageProps) => {
   return (
     <>
-      <h2 className="text-silver flex items-center justify-center gap-2">
+      <h2 className="text-silver body-m flex items-center justify-center gap-2">
         Player {winner === "player-x" ? "1" : "2"} wins!
       </h2>
-      {/* Winner */}
-      <div
-        className={`flex items-center justify-center gap-2 ${winner === "player-x" ? "text-light-blue" : "text-light-yellow"}`}
+
+      <p
+        className={`heading-m flex items-center justify-center gap-2 ${winner === "player-x" ? "text-light-blue" : "text-light-yellow"}`}
       >
         {winner === "player-x" ? <IconX /> : <IconO />}take the round
-      </div>
+      </p>
     </>
   );
 };

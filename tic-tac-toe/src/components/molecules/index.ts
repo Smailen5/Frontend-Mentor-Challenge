66a34dc52f61b5f67444478e7eb909b7384f348e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 18:45:46 +0200

    ✨ feat: esportare il componente WinnerMessage nell'indice dei componenti
    - Aggiunta l'esportazione del nuovo componente WinnerMessage nell'indice dei componenti molecolari, facilitando l'importazione e l'utilizzo del messaggio del vincitore all'interno dell'applicazione Tic Tac Toe.

--- tic-tac-toe/src/components/molecules/index.ts
+++ tic-tac-toe/src/components/molecules/index.ts
@@ -2,3 +2,4 @@ export { default as CardPlayerTurn } from "./CardPlayerTurn.tsx";
 export { default as Logo } from "./Logo.tsx";
 export { default as SwitchSelection } from "./SwitchSelection.tsx";
 export { default as StatisticsGroup } from "./StatisticsGroup.tsx";
+export { default as WinnerMessage } from "./WinnerMessage.tsx";

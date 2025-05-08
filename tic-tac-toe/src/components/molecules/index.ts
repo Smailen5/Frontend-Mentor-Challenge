44fde3ae67de851f3c515155337002f6243dad5c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 15:21:52 +0200

    ✨ feat: aggiornare l'indice dei componenti per includere StatisticsGroup
    - Aggiunta l'esportazione del componente StatisticsGroup nell'indice dei componenti

--- tic-tac-toe/src/components/molecules/index.ts
+++ tic-tac-toe/src/components/molecules/index.ts
@@ -1,3 +1,4 @@
+export { default as CardPlayerTurn } from "./CardPlayerTurn.tsx";
 export { default as Logo } from "./Logo.tsx";
 export { default as SwitchSelection } from "./SwitchSelection.tsx";
-export { default as CardPlayerTurn } from "./CardPlayerTurn.tsx";
+export { default as StatisticsGroup } from "./StatisticsGroup.tsx";

d93b6a532a615752cdaf701434b09ba57fbcf248
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 15:20:10 +0200

    ✨ feat: esportare il nuovo componente BoxStatistics nell'indice degli atomi
    - Aggiunta l'esportazione del componente BoxStatistics nell'indice degli atomi per facilitare l'importazione e l'utilizzo all'interno dell'applicazione, contribuendo alla visualizzazione delle statistiche del gioco.

--- tic-tac-toe/src/components/atoms/index.ts
+++ tic-tac-toe/src/components/atoms/index.ts
@@ -3,3 +3,4 @@ export { default as Card } from "./Card.tsx";
 export { default as Layout } from "./Layout.tsx";
 export { default as PlayerMarkButton } from "./PlayerMarkButton.tsx";
 export { default as GameButton } from "./GameButton.tsx";
+export { default as BoxStatistics } from "./BoxStatistics.tsx";

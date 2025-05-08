5e0b8a29f19bb4a67a24cb3ac0285c58453db3d9
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 15:04:57 +0200

    ✨ feat: aggiungere il nuovo componente GameButton per l'interfaccia utente
    - Creato il componente GameButton che restituisce un pulsante con stili predefiniti, contribuendo all'interattività e all'aspetto visivo dell'applicazione.

---
+++ tic-tac-toe/src/components/atoms/GameButton.tsx
@@ -0,0 +1,7 @@
+const GameButton = () => {
+  return (
+    <button className="bg-semi-dark-navy size-20 cursor-pointer rounded-lg shadow-[0_8px_0_0_rgba(0,0,0,0.25)]"></button>
+  );
+};
+
+export default GameButton;

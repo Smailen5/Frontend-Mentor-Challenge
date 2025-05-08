b2347d02cb6f933b1f0a0d6cb1d5f74d08b3343a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 19:09:03 +0200

    ♻️ refactor: migliorare la struttura del componente Modal
    - Modificato il componente Modal per includere un contenitore con dimensioni fisse per i figli, migliorando l'allineamento e la presentazione del contenuto all'interno del modal. Questa modifica ottimizza l'aspetto visivo e l'usabilità del componente nell'applicazione Tic Tac Toe.

--- tic-tac-toe/src/components/atoms/Modal.tsx
+++ tic-tac-toe/src/components/atoms/Modal.tsx
@@ -5,8 +5,8 @@ const Modal = ({ children }: ModalProps) => {
     // Overlay
     <div className="absolute inset-0 flex h-screen items-center bg-black/70">
       {/* Modal */}
-      <div className="bg-semi-dark-navy w-full space-y-4 py-10 uppercase">
-        {children}
+      <div className="bg-semi-dark-navy flex w-full items-center justify-center py-10 uppercase">
+        <div className="h-[140px] w-[279px] space-y-4">{children}</div>
       </div>
     </div>
   );

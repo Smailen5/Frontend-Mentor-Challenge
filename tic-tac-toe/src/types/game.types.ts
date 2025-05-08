efe6fd9f4e9396ef37fa62aa82f95ba9b228c4fc
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 16:20:41 +0200

    ✨ feat: aggiungere l'interfaccia GameState per gestire lo stato del gioco
    - Introdotta l'interfaccia GameState nel file game.types.ts, che include le proprietà per gestire la fase del gioco, il giocatore selezionato, il giocatore corrente, la griglia di gioco e le funzioni per modificare lo stato del gioco, migliorando la gestione complessiva dello stato nel progetto Tic Tac Toe.

--- tic-tac-toe/src/types/game.types.ts
+++ tic-tac-toe/src/types/game.types.ts
@@ -1,3 +1,13 @@
 export type GamePhase = "player-selection" | "game" | "result";
 export type Player = "player-x" | "player-o";
 
+export interface GameState {
+  phase: GamePhase;
+  selectedPlayer: Player;
+  currentPlayer: Player;
+  grid: (Player | null)[]; // array di 9 celle
+  setPhase: (phase: GamePhase) => void;
+  setSelectedPlayer: (player: Player) => void;
+  makeMove: (position: number) => void;
+  resetGame: () => void;
+}

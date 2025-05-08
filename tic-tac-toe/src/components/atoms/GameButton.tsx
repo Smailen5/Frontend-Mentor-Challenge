c69d5c93d23a92aa9697e9747c56117d5db3453c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 16:54:18 +0200

    ✨ feat: implementare la logica di gioco nel componente GameButton
    - Aggiunta la gestione del click sul pulsante per effettuare una mossa, integrando il metodo makeMove dallo store di gioco. Migliorata la visualizzazione del pulsante con le icone per i giocatori X e O.

--- tic-tac-toe/src/components/atoms/GameButton.tsx
+++ tic-tac-toe/src/components/atoms/GameButton.tsx
@@ -1,8 +1,20 @@
+import { IconO, IconX } from "../../assets";
+import { useGameStore } from "../../store/gameStore";
 import { GameButtonProps } from "../../types/atoms.types";
 
 const GameButton = ({ position, value }: GameButtonProps) => {
+  const { makeMove } = useGameStore();
+
+  const handleClick = () => {
+    makeMove(position);
+  };
   return (
-    <button className="bg-semi-dark-navy size-20 cursor-pointer rounded-lg shadow-[0_8px_0_0_rgba(0,0,0,0.25)]"></button>
+    <button
+      onClick={handleClick}
+      className="bg-semi-dark-navy size-20 cursor-pointer rounded-lg shadow-[0_8px_0_0_rgba(0,0,0,0.25)]"
+    >
+      {value &&(value === "player-x" ? <IconX /> : <IconO />)}
+    </button>
   );
 };
 

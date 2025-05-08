9c96edd4b02b236fef1980eebb47d9030ee7868d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 15:32:26 +0200

    ✨ feat: aggiornare il componente StatisticsGroup per visualizzare le statistiche del gioco
    - Modificato il componente StatisticsGroup per includere istanze di BoxStatistics con dati specifici per il giocatore, i pareggi e il CPU, migliorando la visualizzazione delle statistiche nel gioco.

--- tic-tac-toe/src/components/molecules/StatisticsGroup.tsx
+++ tic-tac-toe/src/components/molecules/StatisticsGroup.tsx
@@ -3,9 +3,18 @@ import { BoxStatistics } from "../atoms";
 const StatisticsGroup = () => {
   return (
     <>
-      <BoxStatistics />
-      <BoxStatistics />
-      <BoxStatistics />
+      <BoxStatistics className="bg-light-blue">
+        <span>x (you)</span>
+        <span>14</span>
+      </BoxStatistics>
+      <BoxStatistics className="bg-silver">
+        <span>ties</span>
+        <span>32</span>
+      </BoxStatistics>
+      <BoxStatistics className="bg-light-yellow">
+        <span>o (cpu)</span>
+        <span>11</span>
+      </BoxStatistics>
     </>
   );
 };

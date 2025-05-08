61c7392afcea547549151ea52767494818229806
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 18:22:53 +0200

    ✨ feat: aggiornare il componente GameResult per visualizzare il vincitore
    - Aggiunta la logica per mostrare il vincitore del gioco all'interno del componente GameResult, integrando il GameBoard e un overlay per una presentazione chiara del risultato finale. Questa modifica migliora l'esperienza utente mostrando il risultato della partita in modo interattivo.

--- tic-tac-toe/src/components/screens/GameResult.tsx
+++ tic-tac-toe/src/components/screens/GameResult.tsx
@@ -1,5 +1,22 @@
+import { useGameStore } from "../../store/gameStore";
+import GameBoard from "./GameBoard";
+
 const GameResult = () => {
-  return <div>GameResult</div>;
+  const { winner } = useGameStore();
+  return (
+    <div className="relative">
+      <GameBoard />
+      {/* Overlay */}
+      <div className="absolute inset-0 flex h-screen items-center bg-black/70">
+        {/* Modal */}
+        <div className="bg-semi-dark-navy w-full">
+          <h2 className="flex items-center justify-center gap-2">
+            Player {winner === "player-x" ? "1" : "2"} wins!
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GameResult;

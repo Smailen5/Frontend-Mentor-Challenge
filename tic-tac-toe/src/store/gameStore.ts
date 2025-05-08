75023652b9bbcda34cb91489d2eb25a7858a476c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 16:01:54 +0200

    ✨ feat: aggiornare lo store di gioco per gestire lo stato e le mosse dei giocatori
    - Aggiunta la gestione del turno corrente e della griglia di gioco, permettendo ai giocatori di effettuare mosse e aggiornando lo stato del gioco di conseguenza. Implementata la logica per il reset del gioco e il cambio di turno.

--- tic-tac-toe/src/store/gameStore.ts
+++ tic-tac-toe/src/store/gameStore.ts
@@ -6,15 +6,41 @@ type Player = "player-x" | "player-o";
 interface GameState {
   phase: GamePhase;
   selectedPlayer: Player;
+  currentPlayer: Player;
+  grid: (Player | null)[]; // array di 9 celle
   setPhase: (phase: GamePhase) => void;
   setSelectedPlayer: (player: Player) => void;
+  makeMove: (position: number) => void;
   resetGame: () => void;
 }
 
 export const useGameStore = create<GameState>((set) => ({
   phase: "player-selection",
-  selectedPlayer: 'player-x',
+  selectedPlayer: "player-x",
+  currentPlayer: "player-x",
+  grid: Array(9).fill(null),
   setPhase: (phase) => set({ phase }),
   setSelectedPlayer: (player) => set({ selectedPlayer: player }),
-  resetGame: () => set({ phase: "player-selection", selectedPlayer: 'player-x' }),
+  makeMove: (position) =>
+    set((state) => {
+      // se la cella e vuota fai la mossa
+      if (state.grid[position] === null) {
+        const newGrid = [...state.grid]; // crea una nuova copia dell'array grid
+        newGrid[position] = state.currentPlayer; // assegna il simbolo al posto selezionato
+
+        return {
+          grid: newGrid, // aggiorna l'array grid con la nuova mossa
+          currentPlayer:
+            state.currentPlayer === "player-x" ? "player-o" : "player-x", // cambia il turno
+        };
+      }
+      return state;
+    }),
+  resetGame: () =>
+    set({
+      phase: "player-selection",
+      selectedPlayer: "player-x",
+      currentPlayer: "player-x",
+      grid: Array(9).fill(null),
+    }),
 }));

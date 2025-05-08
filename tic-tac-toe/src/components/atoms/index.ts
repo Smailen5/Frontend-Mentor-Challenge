b0f60c7b05b1736a544f574b50315d6c12adeb3f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 15:05:05 +0200

    ✨ feat: esportare il componente GameButton nell'indice degli atomi
    - Aggiunta l'esportazione del componente GameButton nell'indice degli atomi per facilitare l'importazione e l'utilizzo all'interno dell'applicazione, migliorando l'interattività dell'interfaccia utente.

--- tic-tac-toe/src/components/atoms/index.ts
+++ tic-tac-toe/src/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./Button.tsx";
 export { default as Card } from "./Card.tsx";
 export { default as Layout } from "./Layout.tsx";
 export { default as PlayerMarkButton } from "./PlayerMarkButton.tsx";
+export { default as GameButton } from "./GameButton.tsx";

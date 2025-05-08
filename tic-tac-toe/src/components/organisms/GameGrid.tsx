e806ac6a3966a1efa73c2df8dd218f64f06c0701
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-08 17:50:54 +0200

    ✨ feat: aggiornare il componente GameGrid per visualizzare la griglia di gioco
    - Integrato l'uso dello store di gioco per recuperare lo stato della griglia e renderizzare i pulsanti di gioco in base ai valori delle celle, migliorando l'interattività e la visualizzazione del gioco Tic Tac Toe.

--- tic-tac-toe/src/components/organisms/GameGrid.tsx
+++ tic-tac-toe/src/components/organisms/GameGrid.tsx
@@ -1,5 +1,15 @@
+import { useGameStore } from "../../store/gameStore";
+import { GameButton } from "../atoms";
+
 const GameGrid = () => {
-  return <div>GameGrid</div>;
+  const { grid } = useGameStore();
+  return (
+    <div className="grid grid-cols-3 justify-items-center gap-x-4 gap-y-10 p-6">
+      {grid.map((cell, index) => (
+        <GameButton key={index} position={index} value={cell} />
+      ))}
+    </div>
+  );
 };
 
 export default GameGrid;

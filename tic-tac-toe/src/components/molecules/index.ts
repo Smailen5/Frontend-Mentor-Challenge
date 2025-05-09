2b6508de907ef06922db618885957eadb8210d7d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-09 16:27:29 +0200

    ✨ feat: esportare il nuovo componente PlayAgainButtons per gestire le azioni di riavvio e avanzamento al turno successivo

--- tic-tac-toe/src/components/molecules/index.ts
+++ tic-tac-toe/src/components/molecules/index.ts
@@ -3,3 +3,4 @@ export { default as Logo } from "./Logo.tsx";
 export { default as SwitchSelection } from "./SwitchSelection.tsx";
 export { default as StatisticsGroup } from "./StatisticsGroup.tsx";
 export { default as WinnerMessage } from "./WinnerMessage.tsx";
+export { default as PlayAgainButtons } from "./PlayAgainButtons.tsx";

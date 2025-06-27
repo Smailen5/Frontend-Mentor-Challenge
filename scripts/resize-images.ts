aee0b69070b79b7fcbc7fd0db9d1228117ff9e63
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-27 13:41:43 +0200

    feat(frontend-mentor-challenges): ✨ aggiunto script per il ridimensionamento delle immagini
    - Creato un nuovo script "resize-images.ts" per ridimensionare le immagini in formato .webp nella directory di input, mantenendo solo quelle più grandi di 480x300 pixel.
    - Implementata la libreria "sharp" per gestire l'elaborazione delle immagini e fornire feedback sul processo di ridimensionamento.

---
+++ scripts/resize-images.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+
+// Percorsi
+const INPUT_DIR = path.join(__dirname, "../screen-capture")
+const OUTPUT_DIR = INPUT_DIR // cambialo in "../screen-capture/preview" in futuro
+
+const resizeImage = async (file: string) =>{
+  const inputPath = path.join(INPUT_DIR, file)
+  const outputPath = path.join(OUTPUT_DIR, file)
+
+  const image = sharp(inputPath)
+  const metadata = await image.metadata()
+
+  // Ridimensiona solo se e' piu' grande di 480x300
+  if ((metadata.width ?? 0) > 480 || (metadata.height ?? 0) > 300) {
+    await image.resize(480, 300).toFile(outputPath)
+    console.log("✅ Ridimensionato: ", file)
+  } else {
+    console.log("❌ Non ridimensionato: ", file)
+  }
+}
+
+fs.readdirSync(INPUT_DIR).filter(file => file.endsWith('.webp')).forEach(file => resizeImage(file))

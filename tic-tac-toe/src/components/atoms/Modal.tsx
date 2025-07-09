cfab3a9160e718bb54bb96d8d59f536e0d58fb0a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-07-09 15:11:59 +0200

    style(tic-tac-toe):💄 aggiornare il componente Modal per migliorare la responsività
    - Modificata la classe CSS del contenitore del modal per utilizzare min-width invece di width, migliorando la responsività e l'aspetto visivo del componente.

--- tic-tac-toe/src/components/atoms/Modal.tsx
+++ tic-tac-toe/src/components/atoms/Modal.tsx
@@ -6,7 +6,7 @@ const Modal = ({ children }: ModalProps) => {
     <div className="absolute inset-0 flex h-screen items-center bg-black/70">
       {/* Modal */}
       <div className="bg-semi-dark-navy flex w-full items-center justify-center py-10 uppercase">
-        <div className="h-[140px] w-[279px] space-y-4">{children}</div>
+        <div className="h-[140px] min-w-[279px] space-y-4">{children}</div>
       </div>
     </div>
   );

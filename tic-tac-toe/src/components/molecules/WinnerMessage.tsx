20f6d6a5b02289152a58526697edeb194ed53f79
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-07-09 15:11:05 +0200

    ✨ feat(tic-tac-toe): aggiornare il messaggio del vincitore per gestire il pareggio
    - Modificato il componente WinnerMessage per visualizzare un messaggio di pareggio e un'opzione per riavviare il gioco.
    - Ottimizzato il codice per una migliore coerenza stilistica e chiarezza nella logica di visualizzazione del vincitore.

--- tic-tac-toe/src/components/molecules/WinnerMessage.tsx
+++ tic-tac-toe/src/components/molecules/WinnerMessage.tsx
@@ -1,18 +1,22 @@
-import { IconO, IconX } from "../../assets";
-import { WinnerMessageProps } from "../../types/molecules.types";
+import { IconO, IconX } from '../../assets';
+import { WinnerMessageProps } from '../../types/molecules.types';
 
 const WinnerMessage = ({ winner }: WinnerMessageProps) => {
   return (
     <>
-      <h2 className="text-silver body-m flex items-center justify-center gap-2">
-        Player {winner === "player-x" ? "1" : "2"} wins!
+      <h2 className='text-silver body-m flex items-center justify-center gap-2'>
+        {winner === 'tie'
+          ? 'restart game?'
+          : `Player ${winner === 'player-x' ? '1' : '2'} wins!`}
       </h2>
 
-      <p
-        className={`heading-m flex items-center justify-center gap-2 ${winner === "player-x" ? "text-light-blue" : "text-light-yellow"}`}
-      >
-        {winner === "player-x" ? <IconX /> : <IconO />}take the round
-      </p>
+      {winner !== 'tie' ? (
+        <p
+          className={`heading-m flex items-center justify-center gap-2 ${winner === 'player-x' ? 'text-light-blue' : 'text-light-yellow'}`}
+        >
+          {winner === 'player-x' ? <IconX /> : <IconO />}take the round
+        </p>
+      ) : null}
     </>
   );
 };

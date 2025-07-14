4bb96550031824acec6b9a94c88c26bbabe5f4e6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-07-14 11:48:36 +0200

    refactor(tic-tac-toe): ♻️ aggiornare il messaggio del vincitore per migliorare la visualizzazione del pareggio
    - Sposta il messaggio di pareggio nel secondo blocco.
    - Stile migliorato per una migliore visualizzazione del messaggio.

--- packages/tic-tac-toe/src/components/molecules/WinnerMessage.tsx
+++ packages/tic-tac-toe/src/components/molecules/WinnerMessage.tsx
@@ -5,18 +5,20 @@ const WinnerMessage = ({ winner }: WinnerMessageProps) => {
   return (
     <>
       <h2 className='text-silver body-m flex items-center justify-center gap-2'>
-        {winner === 'tie'
-          ? 'restart game?'
-          : `Player ${winner === 'player-x' ? '1' : '2'} wins!`}
+        {winner !== 'tie'
+          ? `Player ${winner === 'player-x' ? '1' : '2'} wins!`
+          : null}
       </h2>
 
-      {winner !== 'tie' ? (
+      {winner === 'tie' ? (
+        <h2 className='heading-m text-silver flex justify-center'>restart game?</h2>
+      ) : (
         <p
           className={`heading-m flex items-center justify-center gap-2 ${winner === 'player-x' ? 'text-light-blue' : 'text-light-yellow'}`}
         >
           {winner === 'player-x' ? <IconX /> : <IconO />}take the round
         </p>
-      ) : null}
+      )}
     </>
   );
 };
